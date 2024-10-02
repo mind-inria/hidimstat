710cdd07b28cc8ebe4f2d664acccda7667e90f49
paillarj <joseph.paillard@roche.com>
2024-10-02 14:45:27 +0200

    add scoring method to VIM methods

--- hidimstat/permutation_importance.py
+++ hidimstat/permutation_importance.py
@@ -91,11 +91,11 @@ class PermutationImportance(BaseEstimator, TransformerMixin):
         output_dict["loss_reference"] = loss_reference
         output_dict["loss_perm"] = dict()
 
-        def joblib_predict_one_gp(estimator, X, y, j):
+        def joblib_predict_one_gp(X, j):
             """
             Compute the importance score for a single group of covariates.
             """
-            list_loss_perm = []
+            list_y_pred_perm = []
             X_j = X[:, self.groups[j]].copy()
             X_minus_j = np.delete(X, self.groups[j], axis=1)
             group_ids = self.groups[j]
@@ -111,21 +111,60 @@ class PermutationImportance(BaseEstimator, TransformerMixin):
                     y_pred_perm = self.estimator.predict_proba(X_perm)
                 else:
                     y_pred_perm = self.estimator.predict(X_perm)
-                list_loss_perm.append(self.loss(y_true=y, y_pred=y_pred_perm))
-            return np.array(list_loss_perm)
+
+                list_y_pred_perm.append(y_pred_perm)
+            return np.array(list_y_pred_perm)
 
         # Parallelize the computation of the importance scores for each group
         out_list = Parallel(n_jobs=self.n_jobs)(
-            delayed(joblib_predict_one_gp)(self.estimator, X, y, j)
-            for j in range(len(self.groups))
+            delayed(joblib_predict_one_gp)(X, j) for j in range(len(self.groups))
         )
 
-        for j, list_loss_perm in enumerate(out_list):
-            output_dict["loss_perm"][j] = list_loss_perm
+        return np.stack(out_list, axis=0)
+
+    def score(self, X, y):
+        """
+        Compute the importance scores for each group of covariates.
+
+        Parameters
+        ----------
+        X: array-like of shape (n_samples, n_features)
+            The input samples.
+        y: array-like of shape (n_samples,)
+            The target values.
+
+        Returns
+        -------
+        out_dict: dict
+            A dictionary containing the following keys:
+            - 'loss_reference': the loss of the model with the original data.
+            - 'loss_perm': a dictionary containing the loss of the model with
+            the permuted data for each group.
+            - 'importance': the importance scores for each group.
+        """
+        check_is_fitted(self.estimator)
+
+        output_dict = dict()
+        if self.score_proba:
+            y_pred = self.estimator.predict_proba(X)
+        else:
+            y_pred = self.estimator.predict(X)
+        loss_reference = self.loss(y_true=y, y_pred=y_pred)
+        output_dict["loss_reference"] = loss_reference
+        output_dict["loss_perm"] = dict()
+
+        y_pred_perm = self.predict(X, y)
+
+        output_dict["loss_perm"] = dict()
+        for j, y_pred_j in enumerate(y_pred_perm):
+            list_loss_perm = []
+            for y_pred_perm in y_pred_j:
+                list_loss_perm.append(self.loss(y_true=y, y_pred=y_pred_perm))
+            output_dict["loss_perm"][j] = np.array(list_loss_perm)
 
         output_dict["importance"] = np.array(
             [
-                np.mean(output_dict["loss_perm"][j] - output_dict["loss_reference"])
+                np.mean(output_dict["loss_perm"][j]) - output_dict["loss_reference"]
                 for j in range(self.nb_groups)
             ]
         )
