9a2680e924018d735192147359a429eb69ae8c41
kusch lionel <lionel.a.kusch@inria.fr>
2024-12-13 14:28:19 +0100

    Create a folder with the estimators

--- hidimstat/__init__.py
+++ hidimstat/__init__.py
@@ -1,7 +1,6 @@
 from .adaptive_permutation_threshold import ada_svr
 from .clustered_inference import clustered_inference, hd_inference
 from .desparsified_lasso import desparsified_group_lasso, desparsified_lasso
-from .Dnn_learner_single import DnnLearnerSingle
 from .ensemble_clustered_inference import ensemble_clustered_inference
 from .knockoff_aggregation import knockoff_aggregation
 from .knockoffs import model_x_knockoff
@@ -23,7 +22,6 @@ __all__ = [
     "dcrt_zero",
     "desparsified_lasso",
     "desparsified_group_lasso",
-    "DnnLearnerSingle",
     "ensemble_clustered_inference",
     "group_reid",
     "hd_inference",
