7ef96287edaf5e3be14ca47a5b486a31a395189b
kusch lionel <lionel.a.kusch@inria.fr>
2024-12-18 11:46:15 +0100

    Fix typo

--- hidimstat/test/test_version.py
+++ hidimstat/test/test_version.py
@@ -1,5 +1,6 @@
+import hidimstat
+
 def test_version():
     """Test that the version is set correctly."""
-    import hidimstat
     assert isinstance(hidimstat.__version__, str)
     assert len(hidimstat.__version__.split(".")) == 3
