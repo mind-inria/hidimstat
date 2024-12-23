b1c14e147c5718afb135906855adb1d2b2a30beb
kusch lionel <lionel.a.kusch@inria.fr>
2024-12-23 14:49:27 +0100

    Fix format

--- examples/methods/ada_svr.py
+++ examples/methods/ada_svr.py
@@ -42,7 +42,8 @@ plot_dataset1D(X=X, y=y, beta=beta)
 #############################################################################
 # Usage the methods
 # -----------------
-# see the API for more details about the optional parameter: :py:func:`hidimstat.ada_svr`
+# see the API for more details about the optional parameter:
+# :py:func:`hidimstat.ada_svr`
 
 beta_hat, scale = ada_svr(X, y, rcond=1e-1)
 
@@ -62,7 +63,8 @@ plot_validate_variable_importance(beta, beta_hat)
 
 #############################################################################
 #
-# The pvalue and the corrected pvalue can help to be confident in the previous results.
+# The pvalue and the corrected pvalue can help to be confident in the previous
+# results.
 
 
 pvalue, pvalue_corrected, one_minus_pvalue, one_minus_pvalue_correlation = (
@@ -73,53 +75,61 @@ plot_pvalue_H0(
 )
 
 #############################################################################
-# The pvalue and the corrected pvalue show that the confidence in the variable of importance
-# is high. The pvalue and the corrected pvalue are close to one.
+# The pvalue and the corrected pvalue show that the confidence in the variable
+# of importance is high. The pvalue and the corrected pvalue are close to one.
 # We can conclude that the variable of importance is estimated in this case.
 
 plot_pvalue_H1(
     beta_hat, pvalue, pvalue_corrected, one_minus_pvalue, one_minus_pvalue_correlation
 )
 #############################################################################
-# The results for the alternative hipothese shows that the confidence for not important variables
-# is high. The 1-pvalues are not significant. However, the corrected 1-pvalue is close to one.
-# We can conclude that the not importance variables are estimated in this case.
+# The results for the alternative hipothese shows that the confidence for not
+# important variables is high. The 1-pvalues are not significant. However, the
+# corrected 1-pvalue is close to one. We can conclude that the not importance
+# variables are estimated in this case.
 
 #############################################################################
 #
 # Principle of the methods
 # ------------------------
-# The ADA-SVR method is a statistical inference procedure that estimates the variable of importance of a Support Vector Regression (SVR).
-# The method is a simplification of the permutation test for SVR (see :py:func:`hidimstat.permutation_test`).
-# The principle is to shuffle the target variable and to estimate the variable of importance with the SVR in order to estimate the distribution of the coefficients of the SVR. For ADA-SVR, the distribution of the coefficients of the SVR is assumed to be a normal distribution centred around zeros. ADA-SVR uses the central limit theorem to estimate the 
-# standard deviation of this normal distribution for each coefficient (see the following figure).
+# The ADA-SVR method is a statistical inference procedure that estimates the
+# variable of importance of a Support Vector Regression (SVR).
+# The method is a simplification of the permutation test for SVR
+# (see :py:func:`hidimstat.permutation_test`).
+# The principle is to shuffle the target variable and to estimate the variable
+# of importance with the SVR in order to estimate the distribution of the
+# coefficients of the SVR. For ADA-SVR, the distribution of the coefficients of
+# the SVR is assumed to be a normal distribution centred around zeros. ADA-SVR
+# uses the central limit theorem to estimate the
+# standard deviation of this normal distribution for each coefficient
+# (see the :ref:<following figure `fig:ada_svr_1`).
 #
 # .. figure:: ../../../examples/figures/ada_svr_1.jpg
 #  :width: 400
 #  :alt: Figure 1 of ::footcite:ct:`Gaonkar et al. 2012 <gaonkar_deriving_2012>`.
 #  :align: center
 #  :name: fig:ada_svr_1
-#   
+#
 #  Figure 1 of :footcite:ct:`Gaonkar et al. 2012 <gaonkar_deriving_2012>`.
 #  **Left**: Concept of support vector machines in 2-D space
 #  **Right**: Permutation testing for support vector machines
-# 
+#
 
 #############################################################################
 # Comparison with the permutation test for SVR for validating the approach
 # ------------------------------------------------------------------------
 #
-estimator = SVR(kernel="linear", epsilon=0., gamma="scale", C=1.0)
+estimator = SVR(kernel="linear", epsilon=0.0, gamma="scale", C=1.0)
 estimator.fit(X, y)
 beta_hat_svr = estimator.coef_
 
 # compare that the coefficiants are the same that the one of SVR
-assert np.max(np.abs(beta_hat-beta_hat_svr.T[:,0])) < 1e-4
+assert np.max(np.abs(beta_hat - beta_hat_svr.T[:, 0])) < 1e-4
 
 #############################################################################
 # This coefficient of SVR is an estimation of the importance of variables.
 # For estimating the confidence interval of the importance of variables,
-# ADA-SVR uses propose to estimate the distribution of the coefficients 
+# ADA-SVR uses propose to estimate the distribution of the coefficients
 # of SVR, instead of using the classical permutation test for this estimation.
 
 #############################################################################
@@ -133,13 +143,19 @@ proba = permutation_test(
 plot_compare_proba_estimated(proba, beta_hat, scale)
 
 #############################################################################
-# **Compare the distribution of the coefficients of SVR with the estimation of the distribution 
-# of the coefficients by ADA-SVR**
-print("ADA-SVR assumes that the normal distribution of the coefficient is center around zeros.\n",
-      "Our estimation is that the maximum deviation is: {:.4f}\n".format(np.max(np.abs(np.mean(proba, axis=0)))),
-      "ADA-SVR provides that the standard deviation the normal distribution for each coefficients.\n",
-      "The maximum of difference between AdA-SVR estimation and our estimation is:{:.4f}".format(
-      np.max(np.abs(scale - np.std(proba, axis=0))/scale)))
+# **Compare the distribution of the coefficients of SVR with the
+# estimation of the distribution of the coefficients by ADA-SVR**
+print(
+    "ADA-SVR assumes that the normal distribution of the coefficient is",
+    "center around zeros.\n",
+    "Our estimation is that the maximum deviation is: {:.4f}\n".format(
+        np.max(np.abs(np.mean(proba, axis=0)))
+    ),
+    "ADA-SVR provides that the standard deviation the normal distribution",
+    "for each coefficients.\n",
+    "The maximum of difference between AdA-SVR estimation and our estimation",
+    "is:{:.4f}".format(np.max(np.abs(scale - np.std(proba, axis=0)) / scale)),
+)
 plt.show()
 
 #############################################################################
