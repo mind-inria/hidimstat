61ca013305da01e10b6f5174ba91d5c2c1461176
kusch lionel <lionel.a.kusch@inria.fr>
2024-12-26 15:42:28 +0100

    Add type in the function

--- hidimstat/ada_svr.py
+++ hidimstat/ada_svr.py
@@ -1,8 +1,16 @@
 import numpy as np
+import pandas as pd
 from hidimstat.stat_tools import pval_from_scale
+from typing import Any
 
 
-def ada_svr(X, y, rcond=1e-3):
+def ada_svr(
+    X: np.ndarray[Any, np.dtype[np.float64]],
+    y: np.ndarray[Any, np.dtype[np.float64]],
+    rcond: float = 1e-3,
+) -> tuple[
+    np.ndarray[Any, np.dtype[np.float64]], np.ndarray[Any, np.dtype[np.float64]]
+]:
     """
     ADA-SVR: Adaptive Permutation Threshold Support Vector Regression
 
@@ -35,25 +43,40 @@ def ada_svr(X, y, rcond=1e-3):
     .. footbibliography::
 
     """
-    X = np.asarray(X)
+
+    X_input: np.ndarray[Any, np.dtype[np.float64]] = np.asarray(X)
 
     ## compute matrix C, (see eq.6 of [1])
     # invert matrix X*X'
-    XXT_inv = np.linalg.pinv(np.dot(X, X.T), rcond=rcond)
+    XXT_inv: np.ndarray[Any, np.dtype[np.float64]] = np.linalg.pinv(
+        np.dot(X_input, X_input.T), rcond=rcond
+    )
     # partial computation of the 2nd term of the equation
-    sum_XXT_inv = np.sum(XXT_inv)
-    L = -np.outer(np.sum(XXT_inv, axis=0), np.sum(XXT_inv, axis=1)) / sum_XXT_inv
-    C = np.dot(X.T, XXT_inv + L)
+    sum_XXT_inv: np.ndarray[Any, np.dtype[np.float64]] = np.sum(XXT_inv)
+    L: np.ndarray[Any, np.dtype[np.float64]] = (
+        -np.outer(np.sum(XXT_inv, axis=0), np.sum(XXT_inv, axis=1)) / sum_XXT_inv
+    )
+    C: np.ndarray[Any, np.dtype[np.float64]] = np.dot(X_input.T, XXT_inv + L)
 
     ## compute vector W, (see eq.4 of [1])
-    beta_hat = np.dot(C, y)
+    beta_hat: np.ndarray[Any, np.dtype[np.float64]] = np.dot(C, y)
     ## compute standard deviation of the distribution of W, (see eq.12 of [1])
-    scale = np.sqrt(np.sum(C**2, axis=1))
+    scale: np.ndarray[Any, np.dtype[np.float64]] = np.sqrt(np.sum(C**2, axis=1))
 
     return beta_hat, scale
 
 
-def ada_svr_pvalue(beta_hat, scale, distrib="norm", eps=1e-14):
+def ada_svr_pvalue(
+    beta_hat: np.ndarray[Any, np.dtype[np.float64]],
+    scale: np.ndarray[Any, np.dtype[np.float64]],
+    distrib: str = "norm",
+    eps: float = 1e-14,
+) -> tuple[
+    np.ndarray[Any, np.dtype[np.float64]],
+    np.ndarray[Any, np.dtype[np.float64]],
+    np.ndarray[Any, np.dtype[np.float64]],
+    np.ndarray[Any, np.dtype[np.float64]],
+]:
     """
     Computing one-sided p-values corrrected for multiple testing
     from simple testing one-sided p-values.
