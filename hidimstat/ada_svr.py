2f71f8ba8f2a783eb69aaa9531f1f8de7e954287
kusch lionel <lionel.a.kusch@inria.fr>
2024-12-26 18:04:00 +0100

    Format file and fix import

--- hidimstat/ada_svr.py
+++ hidimstat/ada_svr.py
@@ -44,9 +44,11 @@ def ada_svr(
 
     """
     "X must be a numpy array or a list or a pandas DataFrame"
-    assert (isinstance(X, np.ndarray) or isinstance(X, list)) or isinstance(X, pd.DataFrame)
+    assert (isinstance(X, np.ndarray) or isinstance(X, list)) \
+        or isinstance(X, pd.DataFrame)
     "y must be a numpy array or a list or a pandas DataFrame"
-    assert (isinstance(y, np.ndarray) or isinstance(y, list)) or isinstance(y, pd.DataFrame)
+    assert (isinstance(y, np.ndarray) or isinstance(y, list)) \
+        or isinstance(y, pd.DataFrame)
     assert X.shape[0] == y.shape[0], "X and y must have the same number of samples"
     assert X.ndim == 2, "X must be a 2D array"
     assert y.ndim == 1, "y must be a 1D array"
@@ -74,10 +76,14 @@ def ada_svr(
     ## compute standard deviation of the distribution of W, (see eq.12 of [1])
     scale: np.ndarray[Any, np.dtype[np.float64]] = np.sqrt(np.sum(C**2, axis=1))
 
-    assert beta_hat.shape[0] == X.shape[1], "beta_hat must have the same number of features as X"
+    assert (
+        beta_hat.shape[0] == X.shape[1]
+    ), "beta_hat must have the same number of features as X"
     assert beta_hat.ndim == 1, "beta_hat must be a 1D array"
     assert np.all(np.isfinite(beta_hat)), "beta_hat must be finite values"
-    assert scale.shape[0] == X.shape[1], "scale must have the same number of features as X"
+    assert (
+        scale.shape[0] == X.shape[1]
+    ), "scale must have the same number of features as X"
     assert scale.ndim == 1, "scale must be a 1D array"
     assert np.all(scale >= 0), "scale must be positive"
     assert np.all(np.isfinite(scale)), "scale must be finite values"
