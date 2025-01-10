a0d53b02b1c992e62ce90418b5ba1faa378a061b
kusch lionel <lionel.a.kusch@inria.fr>
2025-01-10 17:53:01 +0100

    Fix test for new signature of the function

--- test/test_knockoff.py
+++ test/test_knockoff.py
@@ -1,4 +1,6 @@
-from hidimstat import knockoff_aggregation, model_x_knockoff
+from hidimstat.knockoffs import model_x_knockoff_aggregation, model_x_knockoff,\
+    model_x_knockoff_filter, model_x_knockoff_pvalue, model_x_knockoff_bootstrap_e_value,\
+    model_x_knockoff_bootstrap_quantile
 from hidimstat.gaussian_knockoff import gaussian_knockoff_generation
 from hidimstat.data_simulation import simu_data
 from hidimstat.utils import cal_fdp_power
@@ -6,24 +8,25 @@ import numpy as np
 import pytest
 from sklearn.covariance import LedoitWolf, GraphicalLassoCV
 
-n = 500
-p = 100
-snr = 5
-n_bootstraps = 25
-fdr = 0.5
-X, y, _, non_zero_index = simu_data(n, p, snr=snr, seed=0)
+
 
 
 def test_knockoff_aggregation():
-    selected_verbose, aggregated_pval, pvals = knockoff_aggregation(
-        X, y, fdr=fdr, n_bootstraps=n_bootstraps, verbose=True, random_state=0
+    n = 500
+    p = 100
+    snr = 5
+    n_bootstraps = 25
+    fdr = 0.5
+    X, y, _, non_zero_index = simu_data(n, p, snr=snr, seed=0)
+    
+    test_scores = model_x_knockoff_aggregation(
+        X, y, n_bootstraps=n_bootstraps, random_state=0
     )
-
+    selected_verbose, aggregated_pval, pvals = model_x_knockoff_bootstrap_quantile(test_scores, fdr=fdr, selection_only=False)
+    
     fdp_verbose, power_verbose = cal_fdp_power(selected_verbose, non_zero_index)
 
-    selected_no_verbose = knockoff_aggregation(
-        X, y, fdr=fdr, n_bootstraps=n_bootstraps, verbose=False, random_state=None
-    )
+    selected_no_verbose = model_x_knockoff_bootstrap_quantile(test_scores, fdr=fdr, selection_only=True)
 
     fdp_no_verbose, power_no_verbose = cal_fdp_power(
         selected_no_verbose, non_zero_index
@@ -34,52 +37,21 @@ def test_knockoff_aggregation():
     assert power_verbose > 0.1
     assert fdp_no_verbose < 0.5
     assert power_no_verbose > 0.1
+    np.testing.assert_array_equal(selected_no_verbose, selected_verbose)
 
     # Single AKO (or vanilla KO) (verbose vs no verbose)
-    selected_no_verbose = knockoff_aggregation(
-        X, y, fdr=fdr, verbose=False, n_bootstraps=1, random_state=5
+    test_bootstrap = model_x_knockoff_aggregation(
+        X, y, n_bootstraps=2, random_state=5
     )
+    test_no_bootstrap = model_x_knockoff(X, y, seed=np.random.RandomState(5).randint(1, np.iinfo(np.int32).max, 2)[0])
 
-    selected_verbose, pvals = knockoff_aggregation(
-        X, y, fdr=fdr, verbose=True, n_bootstraps=1, random_state=5
-    )
-
-    selected_ko = model_x_knockoff(X, y, fdr=fdr, seed=5)
-
-    np.testing.assert_array_equal(selected_no_verbose, selected_ko)
-
-    fdp_no_verbose, power_no_verbose = cal_fdp_power(
-        selected_no_verbose, non_zero_index
-    )
-    fdp_verbose, power_verbose = cal_fdp_power(selected_verbose, non_zero_index)
-
-    assert fdp_verbose < 0.5
-    assert power_verbose > 0.1
+    np.testing.assert_array_equal(test_bootstrap[0], test_no_bootstrap)
 
     # Using e-values aggregation (verbose vs no verbose)
 
-    selected_verbose, aggregated_pval, pvals = knockoff_aggregation(
-        X,
-        y,
-        fdr=fdr,
-        n_bootstraps=n_bootstraps,
-        method="e-values",
-        verbose=True,
-        random_state=0,
-    )
-
+    selected_verbose, aggregated_eval, evals = model_x_knockoff_bootstrap_e_value(test_scores,  fdr=fdr, selection_only=False)
     fdp_verbose, power_verbose = cal_fdp_power(selected_verbose, non_zero_index)
-
-    selected_no_verbose = knockoff_aggregation(
-        X,
-        y,
-        fdr=fdr,
-        n_bootstraps=n_bootstraps,
-        method="e-values",
-        verbose=False,
-        random_state=None,
-    )
-
+    selected_no_verbose = model_x_knockoff_bootstrap_e_value(test_scores,  fdr=fdr, selection_only=True)
     fdp_no_verbose, power_no_verbose = cal_fdp_power(
         selected_no_verbose, non_zero_index
     )
@@ -92,7 +64,7 @@ def test_knockoff_aggregation():
 
     # Checking wrong type for random_state
     with pytest.raises(Exception):
-        _ = knockoff_aggregation(
+        _ = model_x_knockoff_aggregation(
             X,
             y,
             random_state="test",
@@ -100,30 +72,26 @@ def test_knockoff_aggregation():
 
     # Checking value for offset not belonging to (0,1)
     with pytest.raises(Exception):
-        _ = knockoff_aggregation(
-            X,
-            y,
+        _ = model_x_knockoff_bootstrap_quantile(
+            test_scores,
             offset=2,
-            method="quantile",
-            random_state="test",
         )
 
     with pytest.raises(Exception):
-        _ = knockoff_aggregation(
-            X,
-            y,
+        _ = model_x_knockoff_bootstrap_e_value(
+            test_scores,
             offset=2,
-            method="e-values",
-            random_state="test",
         )
 
 def test_model_x_knockoff():
+    
     seed = 42
     fdr = 0.2
     n = 300
     p = 300
     X, y, _, non_zero = simu_data(n, p, seed=seed)
-    ko_result = model_x_knockoff(X, y, fdr=fdr, seed=seed + 1)
+    test_score = model_x_knockoff(X, y, seed=seed + 1)
+    ko_result = model_x_knockoff_filter(test_score, fdr=fdr,)
     fdp, power = cal_fdp_power(ko_result, non_zero)
 
     assert fdp <= 0.2
@@ -131,6 +99,9 @@ def test_model_x_knockoff():
 
 
 def test_estimate_distribution():
+    """
+    TODO replace the estimate distribution by calling knockoff function with them
+    """
     SEED = 42
     fdr = 0.1
     n = 100
