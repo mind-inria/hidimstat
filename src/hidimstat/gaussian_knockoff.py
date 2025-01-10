4bb9eb46a7a3205623204842540ed2b238f60c6a
kusch lionel <lionel.a.kusch@inria.fr>
2025-01-10 18:26:09 +0100

    Improve the docstring

--- src/hidimstat/gaussian_knockoff.py
+++ src/hidimstat/gaussian_knockoff.py
@@ -4,30 +4,43 @@ import numpy as np
 
 def gaussian_knockoff_generation(X, mu, sigma, seed=None, tol=1e-14, repeat=False):
     """
-    Generate second-order knockoff variables using equi-correlated method.
-    Reference: Candes et al. (2016), Barber et al. (2015)
-    
-    original impelmentation:
-    https://github.com/msesia/knockoff-filter/blob/master/R/knockoff/R/create_gaussian.R
-    
+    Generate second-order knockoff variables using the equi-correlated method.
+
+    This function generates knockoff variables for a given design matrix X,
+    using the equi-correlated method described in :cite:`barber2015controlling` and
+    :cite:`candes2018panning`. The function takes as input the design matrix X,
+    the vector of empirical mean values mu, and the empirical covariance
+    matrix sigma. It returns the knockoff variables X_tilde.
+
     Parameters
     ----------
     X: 2D ndarray (n_samples, n_features)
-        original design matrix
+        The original design matrix.
 
     mu : 1D ndarray (n_features, )
-        vector of empirical mean values
-
-    method: str
-        method to generate gaussian knockoff
+        A vector of empirical mean values.
 
     sigma : 2D ndarray (n_samples, n_features)
-        empirical covariance matrix
+        The empirical covariance matrix.
+
+    seed : int, optional
+        A random seed for generating the uniform noise used to create the knockoff variables.
+
+    tol : float, optional
+        A tolerance value used for numerical stability in the calculation of the Cholesky decomposition.
+
+    repeat : bool, optional
+        If True, the function returns the values used to generate the knockoff variables (Mu_tilde and sigma_tilde_decompose),
+        which can be used to generate additional knockoff variables without having to recompute them.
 
     Returns
     -------
     X_tilde : 2D ndarray (n_samples, n_features)
-        knockoff design matrix
+        The knockoff variables.
+
+    References
+    ----------
+    .. footbibliography::
     """
     n_samples, n_features = X.shape
 
@@ -43,7 +56,7 @@ def gaussian_knockoff_generation(X, mu, sigma, seed=None, tol=1e-14, repeat=Fals
     Mu_tilde = X - np.dot(X - mu, Sigma_inv_s)
     # To calculate the Cholesky decomposition later on
     sigma_tilde = 2 * Diag_s - Diag_s.dot(Sigma_inv_s.dot(Diag_s)) #TODO extra operation Sigma_inv_s.dot(Diag_s) ??????
-    # test is the matrix is positive definite 
+    # test is the matrix is positive definite
     while not np.all(np.linalg.eigvalsh(sigma_tilde) > tol): 
         sigma_tilde += 1e-10 * np.eye(n_features)
         warnings.warn(
@@ -62,7 +75,33 @@ def gaussian_knockoff_generation(X, mu, sigma, seed=None, tol=1e-14, repeat=Fals
 
 
 def repeat_gaussian_knockoff_generation(Mu_tilde, sigma_tilde_decompose, seed):
+    """
+    Generate additional knockoff variables using pre-computed values.
+
+    This function generates additional knockoff variables using pre-computed values returned by the
+    gaussian_knockoff_generation function with repeat=True. It takes as input Mu_tilde and
+    sigma_tilde_decompose, which were returned by gaussian_knockoff_generation, and a random seed.
+    It returns the new knockoff variables X_tilde.
+
+    Parameters
+    ----------
+    Mu_tilde : 2D ndarray (n_samples, n_features)
+        The matrix of means used to generate the knockoff variables, returned by gaussian_knockoff_generation.
+
+    sigma_tilde_decompose : 2D ndarray (n_features, n_features)
+        The Cholesky decomposition of the covariance matrix used to generate the knockoff variables,
+        returned by gaussian_knockoff_generation.
+
+    seed : int
+        A random seed for generating the uniform noise used to create the knockoff variables.
+
+    Returns
+    -------
+    X_tilde : 2D ndarray (n_samples, n_features)
+        The knockoff variables.
+    """
     n_samples, n_features = Mu_tilde.shape
+    
     # create a uniform noise for all the data
     rng = np.random.RandomState(seed)
     U_tilde = rng.randn(n_samples, n_features)
@@ -73,21 +112,31 @@ def repeat_gaussian_knockoff_generation(Mu_tilde, sigma_tilde_decompose, seed):
 
 
 def _s_equi(sigma, tol=1e-14):
-    """Estimate diagonal matrix of correlation between real and knockoff
-    variables using equi-correlated equation
-    
-    original implementation: 
-    https://github.com/msesia/knockoff-filter/blob/master/R/knockoff/R/solve_equi.R
+    """
+    Estimate the diagonal matrix of correlation between real and knockoff variables using the equi-correlated equation.
+
+    This function estimates the diagonal matrix of correlation between real and knockoff variables using the
+    equi-correlated equation described in :cite:`barber2015controlling` and
+    :cite:`candes2018panning`. It takes as input the empirical covariance matrix sigma and a tolerance value tol, 
+    and returns a vector of diagonal values of the estimated matrix diag{s}.
 
     Parameters
     ----------
     sigma : 2D ndarray (n_features, n_features)
-        empirical covariance matrix calculated from original design matrix
+        The empirical covariance matrix calculated from the original design matrix.
+
+    tol : float, optional
+        A tolerance value used for numerical stability in the calculation of the eigenvalues of the correlation matrix.
 
     Returns
     -------
     1D ndarray (n_features, )
-        vector of diagonal values of estimated matrix diag{s}
+        A vector of diagonal values of the estimated matrix diag{s}.
+
+    Raises
+    ------
+    Exception
+        If the covariance matrix is not positive-definite.
     """
     n_features = sigma.shape[0]
 
@@ -96,28 +145,26 @@ def _s_equi(sigma, tol=1e-14):
     features_std = np.sqrt(np.diag(sigma))
     scale = np.outer(features_std, features_std)
     corr_matrix = sigma / scale
-    
-    
+
     eig_value = np.linalg.eigvalsh(corr_matrix)
     lambda_min = np.min(eig_value[0])
     # check if the matrix is positive-defined
     if lambda_min < 0:
         raise Exception('The covariance matrix is not positive-definite.')
-    
-    S = np.ones(n_features) * min(2 * lambda_min, 1)
 
+    S = np.ones(n_features) * min(2 * lambda_min, 1)
+    
     psd = np.all(np.linalg.eigvalsh(2 * corr_matrix - np.diag(S)) > tol)
     s_eps = 0
-
     while psd is False:
         if s_eps == 0:
             s_eps = np.eps  # avoid zeros
-        else:
+            s_eps = np.eps
             s_eps *= 10
         # if all eigval > 0 then the matrix is positive define
         psd = np.all(np.linalg.eigvalsh(2 * corr_matrix - np.diag(S * (1 - s_eps))) > tol)
 
-
     S = S * (1 - s_eps)
 
     return S * np.diag(sigma)
+
