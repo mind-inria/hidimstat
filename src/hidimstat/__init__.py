dc6327657b776e5df1f22bd81aebe1cccc5b9d7e
kusch lionel <lionel.a.kusch@inria.fr>
2025-01-13 09:55:27 +0100

    Modify function standarze svr

--- src/hidimstat/__init__.py
+++ src/hidimstat/__init__.py
@@ -9,7 +9,7 @@ from .multi_sample_split import aggregate_quantiles
 from .noise_std import group_reid, reid
 from .permutation_test import permutation_test_cv
 from .scenario import multivariate_1D_simulation
-from .standardized_svr import standardized_svr
+from .empirical_thresholding import standardized_svr
 from .stat_tools import zscore_from_pval
 from .cpi import CPI
 from .loco import LOCO
