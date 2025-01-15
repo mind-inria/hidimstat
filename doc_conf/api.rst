ab0ebd5af70c7a5a500be4306036ba6f0b3a8e7d
kusch lionel <lionel.a.kusch@inria.fr>
2025-01-15 16:37:22 +0100

    Update the function

--- doc_conf/api.rst
+++ doc_conf/api.rst
@@ -21,10 +21,9 @@ Functions
    data_simulation
    desparsified_lasso
    desparsified_lasso_pvalue
-   desparsified_group_lasso
    desparsified_group_lasso_pvalue
    ensemble_clustered_inference
-   group_reid
+   reid
    hd_inference
    knockoff_aggregation
    model_x_knockoff
--- src/hidimstat/__init__.py
+++ src/hidimstat/__init__.py
@@ -1,8 +1,8 @@
 from .adaptive_permutation_threshold import ada_svr
 from .clustered_inference import clustered_inference, hd_inference
-from .desparsified_lasso import desparsified_lasso
 from .desparsified_lasso import (
-    desparsified_group_lasso,
+    desparsified_lasso,
+    desparsified_lasso_pvalue,
     desparsified_group_lasso_pvalue,
 )
 from .Dnn_learner_single import DnnLearnerSingle
@@ -10,7 +10,7 @@ from .ensemble_clustered_inference import ensemble_clustered_inference
 from .knockoff_aggregation import knockoff_aggregation
 from .knockoffs import model_x_knockoff
 from .multi_sample_split import aggregate_quantiles
-from .noise_std import group_reid, reid
+from .noise_std import reid
 from .permutation_test import permutation_test_cv
 from .scenario import multivariate_1D_simulation
 from .standardized_svr import standardized_svr
@@ -30,11 +30,11 @@ __all__ = [
     "clustered_inference",
     "dcrt_zero",
     "desparsified_lasso",
-    "desparsified_group_lasso",
+    "desparsified_lasso_pvalue",
     "desparsified_group_lasso_pvalue",
     "DnnLearnerSingle",
     "ensemble_clustered_inference",
-    "group_reid",
+    "reid",
     "hd_inference",
     "knockoff_aggregation",
     "model_x_knockoff",
