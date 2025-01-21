e90c4764d0115739ea7ddf94cbee146a8e9a697a
kusch lionel <lionel.a.kusch@inria.fr>
2025-01-21 18:38:42 +0100

    Improve coverage

--- test/test_permutation_importance_function.py
+++ test/test_permutation_importance_function.py
@@ -15,7 +15,7 @@ def test_permutation_importance_no_fitting(linear_scenario):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
     regression_model = LinearRegression()
-    importance = permutation_importance(
+    importance, list_loss_j, loss_reference = permutation_importance(
         X_test,
         y_test,
         estimator=regression_model,
@@ -39,8 +39,28 @@ def test_permutation_importance_no_fitting(linear_scenario):
     X_df = pd.DataFrame(X, columns=[f"col_{i}" for i in range(X.shape[1])])
     X_train_df, X_test_df, y_train, y_test = train_test_split(X_df, y, random_state=0)
     regression_model = LinearRegression()
-    importance = permutation_importance(
+    importance, list_loss_j, loss_reference = permutation_importance(
         X_test_df,
+        y_test,
+        estimator=regression_model,
+        n_permutations=20,
+        method="predict",
+        random_state=0,
+        n_jobs=1,
+        groups=groups,
+    )
+
+    assert importance[0].mean() > importance[1].mean()
+    
+    # Same with groups
+    groups = {
+        "group_0": [i for i in important_features],
+        "the_group_1": [i for i in non_important_features],
+    }
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    regression_model = LinearRegression()
+    importance, list_loss_j, loss_reference = permutation_importance(
+        X_test,
         y_test,
         estimator=regression_model,
         n_permutations=20,
@@ -59,7 +79,7 @@ def test_permutation_importance_no_fitting(linear_scenario):
     _, _, y_train_clf, y_test_clf = train_test_split(X, y_clf, random_state=0)
     logistic_model = LogisticRegression()
 
-    importance_clf = permutation_importance(
+    importance_clf, list_loss_j, loss_reference = permutation_importance(
         X_test,
         y_test_clf,
         estimator=logistic_model,
@@ -83,7 +103,7 @@ def test_with_fitting(linear_scenario):
 
     regression_model = LinearRegression()
     regression_model.fit(X_train, y_train)
-    importance = permutation_importance(
+    importance, list_loss_j, loss_reference = permutation_importance(
         X_test,
         y_test,
         estimator=regression_model,
@@ -98,48 +118,5 @@ def test_with_fitting(linear_scenario):
         importance[important_features].mean()
         > importance[non_important_features].mean()
     )
+    
 
-    # Same with groups and a pd.DataFrame
-    groups = {
-        "group_0": [f"col_{i}" for i in important_features],
-        "the_group_1": [f"col_{i}" for i in non_important_features],
-    }
-    X_df = pd.DataFrame(X, columns=[f"col_{i}" for i in range(X.shape[1])])
-    X_train_df, X_test_df, y_train, y_test = train_test_split(X_df, y, random_state=0)
-
-    regression_model = LinearRegression()
-    regression_model.fit(X_train_df, y_train)
-    importance = permutation_importance(
-        X_test_df,
-        y_test,
-        estimator=regression_model,
-        n_permutations=20,
-        method="predict",
-        random_state=0,
-        n_jobs=1,
-        groups=groups,
-    )
-
-    assert importance[0].mean() > importance[1].mean()
-
-    # Classification case
-    y_clf = np.zeros_like(y)
-    for i, quantile in enumerate(np.arange(0.2, 0.8, 0.2)):
-        y_clf[np.where(y > np.quantile(y, quantile))] = i
-    _, _, y_train_clf, y_test_clf = train_test_split(X, y_clf, random_state=0)
-
-    logistic_model = LogisticRegression()
-    logistic_model.fit(X_train, y_train_clf)
-    importance_clf = permutation_importance(
-        X_test,
-        y_test_clf,
-        estimator=logistic_model,
-        n_permutations=20,
-        method="predict_proba",
-        random_state=0,
-        n_jobs=1,
-        loss=log_loss,
-        groups=None,
-    )
-
-    assert importance_clf.shape == (X.shape[1],)
