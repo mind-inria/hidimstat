983cbcd4b7e6bc584f326a74c6277b05903ba2ea
jpaillard <joseph.paillard@inria.fr>
2025-02-17 09:54:01 +0100

    Special case for regression models with native multioutput support. Differ support for multimodal to a next PR. Differ support for ordinal, for the moment handle them as continuous. Improve test for ConditionalSampling.

--- src/hidimstat/conditional_sampling.py
+++ src/hidimstat/conditional_sampling.py
@@ -1,7 +1,29 @@
+from typing import Union
+
 import numpy as np
 from sklearn.base import check_is_fitted
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import (
+    ElasticNet,
+    ElasticNetCV,
+    Lasso,
+    LassoCV,
+    LinearRegression,
+    Ridge,
+    RidgeCV,
+)
 from sklearn.multioutput import MultiOutputClassifier, MultiOutputRegressor
 from sklearn.utils.validation import check_random_state
+
+NATIVELY_MULTIOUTPUT = Union[
+    ElasticNet,
+    ElasticNetCV,
+    Lasso,
+    LassoCV,
+    LinearRegression,
+    Ridge,
+    RidgeCV,
+]
 
 
 class ConditionalSampler:
@@ -10,9 +32,7 @@ class ConditionalSampler:
         model_regression=None,
         model_binary=None,
         model_categorical=None,
-        model_ordinary=None,
         data_type: str = "auto",
-        imputation_model_multimodal= False,
         random_state=None,
         categorical_max_cardinality=10,
     ):
@@ -21,20 +41,17 @@ class ConditionalSampler:
 
         Parameters
         ----------
-
-        model : object
-            The model used to estimate the conditional distribution.
+        model_regression : object
+            The model to use for continuous data.
+        model_binary : object
+            The model to use for binary data.
+        model_categorical : object
+            The model to use for categorical data.
         data_type : str, default="auto"
             The variable type. Supported types include "auto", "continuous", "binary",
             and "categorical". If "auto", the type is inferred from the cardinality of
             the unique values passed to the `fit` method. For categorical variables, the
             default strategy is to use a one-vs-rest classifier.
-        model_regression : object
-            Only used if `data_type` is "auto". The model used to estimate the
-            conditional for continuous variables.
-        model_classification : object
-            Only used if `data_type` is "auto". The model used to estimate the
-            conditional for binary variables.
         random_state : int
             The random state to use for sampling.
         categorical_max_cardinality : int
@@ -43,55 +60,51 @@ class ConditionalSampler:
 
         """
         self.data_type = data_type
-        if not isinstance(imputation_model_multimodal, list):
-            imputation_model_multimodal = [imputation_model_multimodal for _ in range(4)]
-        else:
-            imputation_model_multimodal = imputation_model_multimodal
+        self.model_regression = model_regression
+        self.model_binary = model_binary
+        self.model_categorical = model_categorical
+
         if data_type == "auto":
-            self.model = (model_regression, model_binary, model_categorical)
-            self.imputation_model_multimodal = imputation_model_multimodal
+            self.model_auto = {
+                "continuous": model_regression,
+                "binary": model_binary,
+                "categorical": model_categorical,
+            }
         elif data_type == "continuous":
             self.model = model_regression
-            self.imputation_model_multimodal = imputation_model_multimodal[0]
         elif data_type == "binary":
             self.model = model_binary
-            self.imputation_model_multimodal = imputation_model_multimodal[1]
         elif data_type == "categorical":
             self.model = model_categorical
-            self.imputation_model_multimodal = imputation_model_multimodal[2]
-        elif data_type == "ordinal":
-            self.model = model_ordinary
-            self.imputation_model_multimodal = imputation_model_multimodal[3]
         else:
             raise ValueError(f"type of data '{data_type}' unknow.")
         self.rng = check_random_state(random_state)
         self.categorical_max_cardinality = categorical_max_cardinality
-
 
     def fit(self, X: np.ndarray, y: np.ndarray):
 
         if self.data_type == "auto":
             if len(np.unique(y)) == 2:
                 self.data_type = "binary"
-                self.model = self.model[1]
-                self.imputation_model_multimodal = self.imputation_model_multimodal[1]
+                self.model = self.model_auto["binary"]
             elif len(np.unique(y)) <= self.categorical_max_cardinality:
                 self.data_type = "categorical"
-                self.model = self.model[2]
-                self.imputation_model_multimodal = self.imputation_model_multimodal[2]
+                self.model = self.model_auto["categorical"]
             else:
                 self.data_type = "continuous"
-                self.model = self.model[0]
-                self.imputation_model_multimodal = self.imputation_model_multimodal[0]
+                self.model = self.model_auto["continuous"]
 
         # Group of variables
-        if ((y.ndim > 1) and (y.shape[1] > 1) and not self.imputation_model_multimodal):
-            if (self.data_type in ["binary", "categorical"]):
+        if (y.ndim > 1) and (y.shape[1] > 1):
+            if self.data_type in ["binary", "categorical"]:
                 self.model = MultiOutputClassifier(self.model)
-            else:
+            elif self.data_type == "continuous" and not isinstance(
+                self.model, NATIVELY_MULTIOUTPUT
+            ):
                 self.model = MultiOutputRegressor(self.model)
-            self.imputation_model_multimodal = True
-
+            self.multioutput_ = True
+        else:
+            self.multioutput_ = False
         self.model.fit(X, y)
 
     def sample(self, X: np.ndarray, y: np.ndarray, n_samples: int = 1) -> np.ndarray:
@@ -114,7 +127,9 @@ class ConditionalSampler:
         """
 
         check_is_fitted(self.model)
-        if (self.data_type in ["binary", "categorical"]) and (not hasattr(self.model, "predict_proba")):
+        if (self.data_type in ["binary", "categorical"]) and (
+            not hasattr(self.model, "predict_proba")
+        ):
             raise AttributeError(
                 "The model must have a `predict_proba` method to be used for categorical or binary data."
             )
@@ -132,7 +147,7 @@ class ConditionalSampler:
             y_pred_proba = self.model.predict_proba(X)
 
             # multioutput case (group of variables)
-            if not self.imputation_model_multimodal:
+            if not self.multioutput_:
                 y_classes = [self.model.classes_]
                 y_pred_proba = [y_pred_proba]
             else:
@@ -140,8 +155,16 @@ class ConditionalSampler:
 
             y_pred_cond = []
             for index, classes in enumerate(y_classes):
-                    y_pred_cond.append(np.stack([
-                        self.rng.choice(classes, p=p, size=n_samples)
-                        for p in y_pred_proba[index]
-                    ], axis=1))
-            return np.stack(y_pred_cond,axis=-1,)
+                y_pred_cond.append(
+                    np.stack(
+                        [
+                            self.rng.choice(classes, p=p, size=n_samples)
+                            for p in y_pred_proba[index]
+                        ],
+                        axis=1,
+                    )
+                )
+            return np.stack(
+                y_pred_cond,
+                axis=-1,
+            )
