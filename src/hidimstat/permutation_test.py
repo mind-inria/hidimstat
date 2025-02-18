349ade80f00c20a66376b355323686ea8736905f
lionel kusch <lionel.a.kusch@inria.fr>
2025-02-18 08:55:04 +0000

    Permutation_test (1/4): add comments and docstring of the functions (#111)
    * Fix gitignore
    
    * Remove not necesaru function
    
    * refactor files
    
    * Add option to pytest for detection of test
    
    * Fix test
    
    * Remove not necessary normalisation
    
    * Comment step_down_maxt algo
    
    * Modify function of the integrate the difference.
    
    * format stat tools
    
    * add formating
    
    * Fixe bug
    
    * Format files
    
    * Fix a bug from the reformating
    
    * Better function for empty matrix
    
    * Add a citation
    
    * Fix format
    
    * Improve actual tests
    
    * Add two tests
    
    * format type
    
    * change default values
    
    * Format file
    
    * Change default argument for parallelization
    
    * Update example
    
    * include all
    
    * Modification folowwing suggestion
    
    * Format file
    
    * Small modification
    
    * Add comment on the non usage of CV
    
    * Format file

--- src/hidimstat/permutation_test.py
+++ src/hidimstat/permutation_test.py
@@ -1,100 +1,20 @@
 import numpy as np
 from joblib import Parallel, delayed
 from sklearn.base import clone
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVR
-from sklearn.utils import _safe_indexing
 
-from hidimstat.stat_tools import pval_from_two_sided_pval_and_sign
+from hidimstat.stat_tools import pval_from_two_sided_pval_and_sign, step_down_max_t
+
+__all__ = ["permutation_test", "permutation_test_pval"]
 
 
-def permutation_test_cv(
-    X,
-    y,
-    n_permutations=1000,
-    C=None,
-    Cs=np.logspace(-7, 1, 9),
-    seed=0,
-    n_jobs=1,
-    verbose=1,
+def permutation_test(
+    X, y, estimator, n_permutations=1000, seed=0, n_jobs=None, verbose=0
 ):
-    """Cross-validated permutation test shuffling the target
-
-    Parameters
-    ----------
-    X : ndarray, shape (n_samples, n_features)
-        Data.
-
-    y : ndarray, shape (n_samples,)
-        Target.
-
-    C : float or None, optional (default=None)
-        If None, the linear SVR regularization parameter is set by cross-val
-        running a grid search on the list of hyper-parameters contained in Cs.
-        Otherwise, the regularization parameter is equal to C.
-        The strength of the regularization is inversely proportional to C.
-
-    Cs : ndarray, optional (default=np.logspace(-7, 1, 9))
-        If C is None, the linear SVR regularization parameter is set by
-        cross-val running a grid search on the list of hyper-parameters
-        contained in Cs.
-
-    n_permutations : int, optional (default=1000)
-        Number of permutations used to compute the survival function
-        and cumulative distribution function scores.
-
-    seed : int, optional (default=0)
-        Determines the permutations used for shuffling the target
-
-    n_jobs : int or None, optional (default=1)
-        Number of CPUs to use during the cross validation.
-
-    verbose: int, optional (default=1)
-        The verbosity level: if non zero, progress messages are printed
-        when computing the permutation stats in parralel.
-        The frequency of the messages increases with the verbosity level.
-
-    Returns
-    -------
-    pval_corr : ndarray, shape (n_features,)
-        p-value corrected for multiple testing, with numerically accurate
-        values for positive effects (ie., for p-value close to zero).
-
-    one_minus_pval_corr : ndarray, shape (n_features,)
-        One minus the corrected p-value, with numerically accurate
-        values for negative effects (ie., for p-value close to one).
     """
+    Permutation test
 
-    if C is None:
-
-        steps = [("SVR", LinearSVR())]
-        pipeline = Pipeline(steps)
-        parameters = {"SVR__C": Cs}
-        grid = GridSearchCV(pipeline, param_grid=parameters, n_jobs=n_jobs)
-        grid.fit(X, y)
-        C = grid.best_params_["SVR__C"]
-        estimator = LinearSVR(C=C)
-
-    else:
-
-        estimator = LinearSVR(C=C)
-
-    pval_corr, one_minus_pval_corr = permutation_test(
-        X,
-        y,
-        estimator,
-        n_permutations=n_permutations,
-        seed=seed,
-        n_jobs=n_jobs,
-        verbose=verbose,
-    )
-
-    return pval_corr, one_minus_pval_corr
-
-
-def permutation_test(X, y, estimator, n_permutations=1000, seed=0, n_jobs=1, verbose=1):
-    """Permutation test shuffling the target
+    This function compute the distribution of the weights of a linear model
+    by shuffling the target :footcite:t:`hirschhorn2005genome`.
 
     Parameters
     ----------
@@ -104,6 +24,9 @@ def permutation_test(X, y, estimator, n_permutations=1000, seed=0, n_jobs=1, ver
     y : ndarray, shape (n_samples,)
         Target.
 
+    estimator : object LinearModel
+        The linear model used to fit the data.
+
     n_permutations : int, optional (default=1000)
         Number of permutations used to compute the survival function
         and cumulative distribution function scores.
@@ -111,38 +34,72 @@ def permutation_test(X, y, estimator, n_permutations=1000, seed=0, n_jobs=1, ver
     seed : int, optional (default=0)
         Determines the permutations used for shuffling the target
 
-    n_jobs : int or None, optional (default=1)
+    n_jobs : int or None, optional (default=None)
         Number of CPUs to use during the cross validation.
 
-    verbose: int, optional (default=1)
-        The verbosity level: if non zero, progress messages are printed
-        when computing the permutation stats in parralel.
-        The frequency of the messages increases with the verbosity level.
+    verbose : int, optional (default=0)
+        The verbosity level of the joblib.Parallel.
 
     Returns
     -------
-    pval_corr : ndarray, shape (n_features,)
-        p-value corrected for multiple testing, with numerically accurate
-        values for positive effects (ie., for p-value close to zero).
+    weights : ndarray, shape (n_features,)
+        The weights of the original model.
 
-    one_minus_pval_corr : ndarray, shape (n_features,)
-        One minus the corrected p-value, with numerically accurate
-        values for negative effects (ie., for p-value close to one).
+    weights_distribution : ndarray, shape (n_permutations, n_features)
+        The distribution of the weights of the model obtained by shuffling
+        the target n_permutations times.
+
+    References
+    ----------
+    .. footbibliography::
+
     """
 
     rng = np.random.default_rng(seed)
 
-    stat = _permutation_test_stat(clone(estimator), X, y)
+    # Get the weights of the original model
+    if not hasattr(estimator, "coef_"):
+        weights = _fit_and_weights(estimator, X, y)
+    else:
+        weights = estimator.coef_
 
-    permutation_stats = Parallel(n_jobs=n_jobs, verbose=verbose)(
-        delayed(_permutation_test_stat)(clone(estimator), X, _shuffle(y, rng))
+    # Get the distribution of the weights by shuffling the target
+    weights_distribution = Parallel(n_jobs=n_jobs, verbose=verbose)(
+        delayed(_fit_and_weights)(clone(estimator), X, _shuffle_vector(y, rng))
         for _ in range(n_permutations)
     )
 
-    permutation_stats = np.array(permutation_stats)
-    two_sided_pval_corr = step_down_max_T(stat, permutation_stats)
+    # Convert the list of weights into an array
+    weights_distribution = np.array(weights_distribution)
 
-    stat_sign = np.sign(stat)
+    return weights, weights_distribution
+
+
+def permutation_test_pval(weights, weights_distribution):
+    """
+    Compute p-value from permutation test
+
+    Parameters
+    ----------
+    weights : ndarray, shape (n_features,)
+        The weights of the original model.
+
+    weights_distribution : ndarray, shape (n_permutations, n_features)
+        The distribution of the weights of the model obtained by shuffling
+
+    Returns
+    -------
+    pval_corr : ndarray, shape (n_features,)
+        p-value corrected for multiple testing, with numerically accurate
+        values for positive effects (ie., for p-value close to zero).
+
+    one_minus_pval_corr : ndarray, shape (n_features,)
+        One minus the corrected p-value, with numerically accurate
+        values for negative effects (ie., for p-value close to one).
+    """
+    two_sided_pval_corr = step_down_max_t(weights, weights_distribution)
+
+    stat_sign = np.sign(weights)
 
     pval_corr, _, one_minus_pval_corr, _ = pval_from_two_sided_pval_and_sign(
         two_sided_pval_corr, stat_sign
@@ -151,65 +108,47 @@ def permutation_test(X, y, estimator, n_permutations=1000, seed=0, n_jobs=1, ver
     return pval_corr, one_minus_pval_corr
 
 
-def _permutation_test_stat(estimator, X, y):
-    """Fit estimator and get coef"""
-    stat = estimator.fit(X, y).coef_
-    return stat
-
-
-def _shuffle(y, rng):
-    """Shuffle vector"""
-    indices = rng.permutation(len(y))
-    return _safe_indexing(y, indices)
-
-
-def step_down_max_T(stat, permutation_stats):
-    """Step-down maxT algorithm for computing adjusted p-values
+def _fit_and_weights(estimator, X, y):
+    """
+    Fit the estimator and return the weights
 
     Parameters
     ----------
-    stat : ndarray, shape (n_features,)
-        Statistic computed on the original (unpermutted) problem.
+    estimator : object
+        The estimator to fit.
 
-    permutation_stats : ndarray, shape (n_permutations, n_features)
-        Statistics computed on permutted problems.
+    X : ndarray, shape (n_samples, n_features)
+        Data.
+
+    y : ndarray, shape (n_samples,)
+        Target.
 
     Returns
     -------
-    two_sided_pval_corr : ndarray, shape (n_features,)
-        Two-sided p-values corrected for multiple testing.
-
-    References
-    ----------
-    .. [1] Westfall, P. H., & Young, S. S. (1993). Resampling-based multiple
-           testing: Examples and methods for p-value adjustment (Vol. 279).
-           John Wiley & Sons.
+    weights : ndarray, shape (n_features,)
+        The weights of the estimator.
     """
+    weights = estimator.fit(X, y).coef_
+    return weights
 
-    n_permutations, n_features = np.shape(permutation_stats)
 
-    index_ordered = np.argsort(np.abs(stat))
-    stat_ranked = np.empty(n_features)
-    stat_ranked[index_ordered] = np.arange(n_features)
-    stat_ranked = stat_ranked.astype(int)
-    stat_sorted = np.copy(np.abs(stat)[index_ordered])
-    permutation_stats_ordered = np.copy(np.abs(permutation_stats)[:, index_ordered])
+def _shuffle_vector(y, rng):
+    """
+    Shuffle the target
 
-    for i in range(1, n_features):
-        permutation_stats_ordered[:, i] = np.maximum(
-            permutation_stats_ordered[:, i - 1], permutation_stats_ordered[:, i]
-        )
+    Parameters
+    ----------
+    y : ndarray, shape (n_samples,)
+        Target.
 
-    two_sided_pval_corr = (
-        np.sum(np.less_equal(stat_sorted, permutation_stats_ordered), axis=0)
-        / n_permutations
-    )
+    rng : numpy.random.Generator
+        Random number generator.
 
-    for i in range(n_features - 1)[::-1]:
-        two_sided_pval_corr[i] = np.maximum(
-            two_sided_pval_corr[i], two_sided_pval_corr[i + 1]
-        )
-
-    two_sided_pval_corr = np.copy(two_sided_pval_corr[stat_ranked])
-
-    return two_sided_pval_corr
+    Returns
+    -------
+    y_shuffled : ndarray, shape (n_samples,)
+        Shuffled target.
+    """
+    y_copy = np.copy(y)
+    rng.shuffle(y_copy)
+    return y_copy
