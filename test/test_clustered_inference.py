2556a26efb2028eacd08c155649bc5a23bddab74
kusch lionel <lionel.a.kusch@inria.fr>
2025-02-20 14:19:13 +0100

    Add a new tests

--- test/test_clustered_inference.py
+++ test/test_clustered_inference.py
@@ -111,3 +111,64 @@ def test_clustered_inference():
     assert_almost_equal(
         pval_corr[extended_support:], expected[extended_support:], decimal=1
     )
+
+# Scenario 3: data with no temporal dimension and with groups
+def test_clustered_inference_no_temporal_groups():
+    """
+    Testing the procedure on one simulations with a 1D data structure and
+    with n << p: no temporal dimension. The support is connected and of
+    size 10, it must be recovered with a small spatial tolerance
+    parametrized by `margin_size`.
+    We group the sample in 10 groups of size 10.
+    Computing one sided p-values, we want low p-values for the features of
+    the support and p-values close to 0.5 for the others.
+    """
+
+    n_samples, n_features = 20, 2000
+    support_size = 10
+    n_groups = 10
+    sigma = 5.0
+    rho = 0.95
+    n_clusters = 200
+    margin_size = 5
+    interior_support = support_size - margin_size
+    extended_support = support_size + margin_size
+
+    # create n_group of samples
+    X_ = []
+    y_ = []
+    for i in range(n_groups):
+        X_init, y, beta, epsilon = multivariate_1D_simulation(
+            n_samples=n_samples,
+            n_features=n_features,
+            support_size=support_size,
+            sigma=sigma,
+            rho=rho,
+            shuffle=False,
+            seed=2 + i,
+        )
+        X_.append(X_init)
+        y_.append(y)
+
+    y_ = np.concatenate(y_)
+    y_ = y_ - np.mean(y_)
+    X_ = np.concatenate(X_)
+    X_ = X_ - np.mean(X_, axis=0)
+    groups = np.repeat(np.arange(0, n_groups), n_samples)
+
+    connectivity = image.grid_to_graph(n_x=n_features, n_y=1, n_z=1)
+    ward = FeatureAgglomeration(
+        n_clusters=n_clusters, connectivity=connectivity, linkage="ward"
+    )
+
+    beta_hat, pval, pval_corr, one_minus_pval, one_minus_pval_corr = (
+        clustered_inference(X_, y_, ward, n_clusters, groups=groups)
+    )
+
+    expected = 0.5 * np.ones(n_features)
+    expected[:support_size] = 0.0
+
+    assert_almost_equal(pval_corr[:interior_support], expected[:interior_support])
+    assert_almost_equal(
+        pval_corr[extended_support:200], expected[extended_support:200], decimal=1
+    )
