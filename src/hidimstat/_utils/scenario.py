6426f59056e29cddc5286e1d54f05b92602544d3
kusch lionel <lionel.a.kusch@inria.fr>
2025-03-10 15:10:46 +0100

    Move the simu_data in scenario

--- src/hidimstat/_utils/scenario.py
+++ src/hidimstat/_utils/scenario.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import ndimage
+from scipy.linalg import toeplitz
 
 ROI_SIZE_2D = 2
 SHAPE_2D = (12, 12)
@@ -302,3 +303,58 @@ def multivariate_temporal_simulation(
     Y = np.dot(X, beta) + noise
 
     return X, Y, beta, noise
+
+
+def multivariate_1D_simulation_AR(n, p, rho=0.25, snr=2.0, sparsity=0.06, effect=1.0, seed=None):
+    """Function to simulate data follow an autoregressive structure with Toeplitz
+    covariance matrix
+
+    Parameters
+    ----------
+    n : int
+        number of observations
+    p : int
+        number of variables
+    sparsity : float, optional
+        ratio of number of variables with non-zero coefficients over total
+        coefficients
+    rho : float, optional
+        correlation parameter
+    effect : float, optional
+        signal magnitude, value of non-null coefficients
+    seed : None or Int, optional
+        random seed for generator
+
+    Returns
+    -------
+    X : ndarray, shape (n, p)
+        Design matrix resulted from simulation
+    y : ndarray, shape (n, )
+        Response vector resulted from simulation
+    beta_true : ndarray, shape (n, )
+        Vector of true coefficient value
+    non_zero : ndarray, shape (n, )
+        Vector of non zero coefficients index
+
+    """
+    # Setup seed generator
+    rng = np.random.default_rng(seed)
+
+    # Number of non-null
+    k = int(sparsity * p)
+
+    # Generate the variables from a multivariate normal distribution
+    mu = np.zeros(p)
+    Sigma = toeplitz(rho ** np.arange(0, p))  # covariance matrix of X
+    # X = np.dot(np.random.normal(size=(n, p)), cholesky(Sigma))
+    X = rng.multivariate_normal(mu, Sigma, size=(n))
+    # Generate the response from a linear model
+    non_zero = rng.choice(p, k, replace=False)
+    beta_true = np.zeros(p)
+    beta_true[non_zero] = effect
+    eps = rng.standard_normal(size=n)
+    prod_temp = np.dot(X, beta_true)
+    noise_mag = np.linalg.norm(prod_temp) / (snr * np.linalg.norm(eps))
+    y = prod_temp + noise_mag * eps
+
+    return X, y, beta_true, non_zero
--- test/test_data_simulation.py
+++ test/test_data_simulation.py
@@ -1,9 +1,9 @@
 import numpy as np
-from hidimstat._utils.data_simulation import simu_data
+from hidimstat._utils.scenario import multivariate_1D_simulation_AR
 
 
 def test_simu_data():
-    X, y, _, _ = simu_data(n=100, p=200, seed=42)
+    X, y, _, _ = multivariate_1D_simulation_AR(n=100, p=200, seed=42)
 
     assert X.shape == (100, 200)
     assert y.size == 100
@@ -12,5 +12,5 @@ def test_simu_data():
 def test_non_zero_index():
     # Test to make sure non-null variable indices are sampled without
     # replacement
-    X, y, _, non_zero = simu_data(10, 10, sparsity=1.0, seed=0)
+    X, y, _, non_zero = multivariate_1D_simulation_AR(10, 10, sparsity=1.0, seed=0)
     assert non_zero.size == np.unique(non_zero).size
