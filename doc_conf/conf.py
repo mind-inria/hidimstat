133c99a4a957fe07ffd9de6735ae86a966696152
Joseph Paillard <joseph.paillard@inria.fr>
2025-03-13 10:47:06 +0100

    Update doc_conf/conf.py
    Co-authored-by: lionel kusch <lionel.a.kusch@inria.fr>

--- doc_conf/conf.py
+++ doc_conf/conf.py
@@ -62,6 +62,8 @@ language = "en"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_logo = "_static/logo.png"
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
 html_theme = "pydata_sphinx_theme"
 html_title = "HiDimStat"
 html_context = {
