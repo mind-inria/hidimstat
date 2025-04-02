0418847f5df4d25e959776584f0e12a8109d946f
lionel kusch <lionel.a.kusch@inria.fr>
2025-04-02 19:04:32 +0200

    Update src/hidimstat/base_perturbation.py
    Co-authored-by: Joseph Paillard <joseph.paillard@inria.fr>

--- src/hidimstat/base_perturbation.py
+++ src/hidimstat/base_perturbation.py
@@ -159,7 +159,7 @@ class BasePerturbation(BaseEstimator):
             or not hasattr(self, "groups")
             or not hasattr(self, "_groups_ids")
         ):
-            raise ValueError("The estimators require to be fit before to use them")
+            raise ValueError("The estimator is not fitted. The fit method must be called to set variable groups. If no grouping is needed, call fit with groups=None")
 
     def _joblib_predict_one_group(self, X, group_id, group_key):
         """
