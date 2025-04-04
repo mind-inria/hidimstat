f7c0537f4195a42d950d6048bbe300eab8802b60
lionel kusch <lionel.a.kusch@inria.fr>
2025-04-04 10:29:43 +0200

    dcrt0(1/4): add comments and docstring of the functions (#133)
    * Add comments to _lambda_alpha_max
    
    * Remove loss parameter
    
    * Add full parameters for the frd threshold
    
    * Add comments
    
    * Remove uppercase letter
    
    * Add bibliography
    
    * Add reference
    
    * Remove sigma_zero
    
    * Fix some typo and function
    
    * Fix bugs in tests
    
    * Fix format
    
    * add link to original implementation
    
    * At todo on difference of implementation
    
    * improve dcrt
    
    * mprove warning message
    
    * Add warning
    
    * Format document
    
    * Add new assert
    
    * Add new function in the init
    
    * Include the new funvtion in the documentation
    
    * Improve comments
    
    * Format file
    
    * formating files
    
    * Update a comment
    
    * Improve coverage
    
    * Format file
    
    * Fix bug in examples
    
    * Split tests
    
    * Format file
    
    * Modify a docstring
    
    * Fix example
    
    * Formating
    
    * Remove unecessary test
    
    * Remove option
    
    * Format
    
    * Remove random generator in LassoCV
    
    The random_generator is important only when the selection is random.
    
    * Change usage of alpha
    
    * format file
    
    * Add a comment
    
    * Add space for testing CI
    
    * fix format
    
    * improve example
    
    * Include modification
    
    * rename some variable
    
    * format document
    
    * change way to pass argument to lasso
    
    * add test for testing dictionnary
    
    * Format
    
    * change name of parameters
    
    * modify randomforest to random_forest
    
    * Update docstring
    
    * Update the description of the tests
    
    * change naem selected_variable
    
    * rename ntree to n_tree
    
    * Remane sigma2

--- src/hidimstat/__init__.py
+++ src/hidimstat/__init__.py
@@ -18,6 +18,7 @@ from .leave_one_covariate_out import LOCO
 from .multi_sample_split import aggregate_quantiles
 from .noise_std import reid
 from .permutation_importance import PermutationImportance
+from .dcrt import dcrt_zero, dcrt_pvalue
 from .permutation_test import permutation_test, permutation_test_pval
 from .scenario import multivariate_1D_simulation
 from .stat_tools import zscore_from_pval
@@ -31,6 +32,7 @@ __all__ = [
     "aggregate_quantiles",
     "clustered_inference",
     "dcrt_zero",
+    "dcrt_pvalue",
     "desparsified_lasso",
     "desparsified_lasso_pvalue",
     "desparsified_group_lasso_pvalue",
