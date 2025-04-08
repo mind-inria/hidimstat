546547ae1024735e190f132b4367c4627ecf1d94
jpaillard <joseph.paillard@inria.fr>
2025-04-08 10:52:22 +0200

    replace ANOVA by d0CRT

--- examples/plot_model_agnostic_importance.py
+++ examples/plot_model_agnostic_importance.py
@@ -1,27 +1,28 @@
 """
-Model agnostic variable importance when ANOVA fails
-======================================================
+Model agnostic variable importance when linear models fail
+==========================================================
 
-In this example, we illustrate on the circles dataset the limitations of variable
-importance methods based on linear models, such as the Analysis of Variance (ANOVA)
-F-test. We then show how model agnostic methods such as Leave-One-Covariate-Out (LOCO)
-allow to compute variable importance even when classes are not linearly separable.
+In this example, we illustrate the limitations of variable selection methods based on
+linear models using the circles dataset. We first use the distilled conditional
+randomization test (d0CRT), which is based on linear models :cite:`liu2022fast` and then
+demonstrate how model-agnostic methods, such as Leave-One-Covariate-Out (LOCO), can
+identify important variables even when classes are not linearly separable.
 
-To compute the importance of a variable, LOCO re-fits a sub-model on a subset of the
-data where the variable of interest is removed. The importance of the variable is then
-computed as the difference between the loss of the sub-model and the loss of the full
-model. As shown in :footcite:t:`williamson2021nonparametric` this loss difference can
-be interpreted as an unnormalized generalized ANOVA (difference of R2). Denoting
-:math:`\mu` the predictive model used, :math:`\mu_{-j}` the sub-model where the j-th
-variable is removed, and :math:`X^{-j}` the data with the j-th variable removed, the
-loss difference can be expressed as:
+To evaluate the importance of a variable, LOCO re-fits a sub-model using a subset of the
+data where the variable of interest is removed. The importance of the variable is
+quantified as the difference in loss between the full model and the sub-model. As shown
+in :cite:t:williamson2021nonparametric, this loss difference can be interpreted as an
+unnormalized generalized ANOVA (difference of R²).  Denoting :math:`\mu` the predictive
+model used, :math:`\mu_{-j}` the sub-model where the j-th variable is removed, and
+:math:`X^{-j}` the data with the j-th variable removed, the loss difference can be
+expressed as:
 .. math:: \psi_{j} = \mathbb{V}(y) \left[\left[ 1 - \frac{\mathbb{E}[(y - \mu(X))^2]}{\mathbb{V}(y)}\right] - \left[ 1 - \frac{\mathbb{E}[(y - \mu_{-j}(X^{-j}))^2]}{\mathbb{V}(y)}\right]\right]
 
 where :math:`\psi_{j}` is the LOCO importance of the j-th variable.
 
 References
 ----------
-.. footbibliography::
+.. bibliography::
 """
 
 import matplotlib.pyplot as plt
@@ -31,18 +32,19 @@ import seaborn as sns
 from scipy.stats import ttest_1samp
 from sklearn.base import clone
 from sklearn.datasets import make_circles
-from sklearn.feature_selection import f_classif
-from sklearn.metrics import hinge_loss
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.metrics import hinge_loss, log_loss
 from sklearn.model_selection import KFold
 from sklearn.svm import SVC
 
-from hidimstat import LOCO
+from hidimstat import LOCO, dcrt_pvalue, dcrt_zero
 
 #############################################################################
 # Generate data where classes are not linearly separable
 # --------------------------------------------------------------
 rng = np.random.RandomState(0)
 X, y = make_circles(n_samples=500, noise=0.1, factor=0.6, random_state=rng)
+
 
 fig, ax = plt.subplots()
 sns.scatterplot(
@@ -59,36 +61,60 @@ plt.show()
 
 
 ###############################################################################
-# Compute variable importance using ANOVA and LOCO
-# -----------------------------------------------------
-# The ANOVA F-test is computed using the f_classif function from sklearn. The LOCO
-# importance is computed using the LOCO class from hidimstat. We then compare the
-# log10 p-values obtained from both methods and represent the commonly used 0.05
-# threshold. The pvalues obtained with an ANOVA suggest that the two features are
-# unimportant. However, the importances obtained by using LOCO with a SVC model
-# reveal that the two features are in fact important.
-_, pval_anova = f_classif(X, y)
-print(f"ANOVA F-test pvalues: {pval_anova}")
+# Compute variable selection using d0CRT and then LOCO
+# ---------------------------------------------------------------------------
+# We first compute the p-values using d0CRT which performs a conditional independence
+# test (:math:`H_0: X_j \perp\!\!\!\perp y | X_{-j}`) for each variable. However,
+# this test is based on a linear model (LogisticRegression) and fails to reject the null
+# in the presence of non-linear relationships. We then compute the p-values using LOCO
+# and show that it can accommodate both linear and non-linear models. When using a
+# misspecified model, such as a linear model for this dataset, LOCO fails to reject the null
+# similarly to d0CRT. However, when using a non-linear model (SVC), LOCO is able to
+# identify the important variables. This is captured by the p-values plotted below
+# along with the commonly used 0.05 significance threshold.
+selection_feat, X_res, sigma2, y_res = dcrt_zero(
+    X,
+    y,
+    problem_type="classification",
+)
+_, pval_dcrt, _ = dcrt_pvalue(
+    selection_features=selection_feat, X_res=X_res, y_res=y_res, sigma2=sigma2, fdr=0.05
+)
 
+# Compute p-values using LOCO
 cv = KFold(n_splits=5, shuffle=True, random_state=0)
-model = SVC(kernel="rbf", random_state=0)
+model_svc = SVC(kernel="rbf", random_state=0)
+model_linear = LogisticRegressionCV(Cs=np.logspace(-3, 3, 5))
 
-importances = []
+importances_linear = []
+importances_svc = []
 for train, test in cv.split(X):
-    model_c = clone(model)
-    model_c.fit(X[train], y[train])
+    model_svc_ = clone(model_svc)
+    model_linear_ = clone(model_linear)
+    model_svc_.fit(X[train], y[train])
+    model_linear_.fit(X[train], y[train])
 
-    vim = LOCO(estimator=model_c, loss=hinge_loss, method="decision_function", n_jobs=2)
-    vim.fit(X[train], y[train])
-    importances.append(vim.score(X[test], y[test])["importance"])
+    vim_linear = LOCO(
+        estimator=model_linear_, loss=log_loss, method="predict_proba", n_jobs=2
+    )
+    vim_svc = LOCO(
+        estimator=model_svc_, loss=hinge_loss, method="decision_function", n_jobs=2
+    )
+    vim_linear.fit(X[train], y[train])
+    vim_svc.fit(X[train], y[train])
 
-_, pval_loco = ttest_1samp(importances, 0, axis=0, alternative="greater")
+    importances_linear.append(vim_linear.score(X[test], y[test])["importance"])
+    importances_svc.append(vim_svc.score(X[test], y[test])["importance"])
+
+
+_, pval_linear = ttest_1samp(importances_linear, 0, axis=0, alternative="greater")
+_, pval_svc = ttest_1samp(importances_svc, 0, axis=0, alternative="greater")
 
 df_pval = pd.DataFrame(
     {
-        "pval": np.hstack([pval_anova, pval_loco]),
-        "method": ["ANOVA"] * len(pval_anova) + ["LOCO"] * len(pval_loco),
-        "Feature": ["X1", "X2"] * 2,
+        "pval": np.hstack([pval_dcrt, pval_linear, pval_svc]),
+        "method": ["d0CRT"] * 2 + ["LOCO-linear"] * 2 + ["LOCO-SVC"] * 2,
+        "Feature": ["X1", "X2"] * 3,
     }
 )
 df_pval["log10pval"] = -np.log10(df_pval["pval"])
