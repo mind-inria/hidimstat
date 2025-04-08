4ddd8de15b97792c4015b5b32430104104a3ff94
kusch lionel <lionel.a.kusch@inria.fr>
2025-04-08 18:55:19 +0200

    improve warning mesage

--- src/hidimstat/gaussian_knockoff.py
+++ src/hidimstat/gaussian_knockoff.py
@@ -181,7 +181,7 @@ def _s_equi(sigma, tol=1e-14):
         )
         warnings.warn(
             "The equi-correlated matrix for knockoffs is not positive "
-            "definite. Reduce the value of distance.",
+            f"definite. Reduce the value of distance by {s_eps}.",
             UserWarning,
         )
 
