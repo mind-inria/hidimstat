2b281502466832025e67b14bde70168a272d458c
kusch lionel <lionel.a.kusch@inria.fr>
2025-04-09 09:44:25 +0200

    increase the number of element

--- examples/plot_knockoff_aggregation.py
+++ examples/plot_knockoff_aggregation.py
@@ -41,9 +41,9 @@ from hidimstat.utils import cal_fdp_power
 plt.rcParams.update({"font.size": 26})
 
 # Number of observations
-n_subjects = 200
+n_subjects = 300
 # Number of variables
-n_clusters = 200
+n_clusters = 300
 # Correlation parameter
 rho = 0.7
 # Ratio of number of variables with non-zero coefficients over total
