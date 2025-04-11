0f07310ab83b475a13e1da2a4c9a1546174eb431
kusch lionel <lionel.a.kusch@inria.fr>
2025-04-11 13:29:39 +0200

    difference

--- examples/plot_knockoff_aggregation.py
+++ examples/plot_knockoff_aggregation.py
@@ -42,137 +42,142 @@ plt.rcParams.update({"font.size": 12})
 
 # Number of observations
 n_subjects = 100
-# Number of variables
-n_clusters = 100
-# Correlation parameter
-rho = 0.3
-# Ratio of number of variables with non-zero coefficients over total
-# coefficients
-sparsity = 0.1
-# Desired controlled False Discovery Rate (FDR) level
-fdr = 0.1
-snr = 10
-seed = 45
-n_bootstraps = 25
-runs = 20
-n_jobs = None
-joblib_verbose = 0
-
-rng = check_random_state(seed)
-seed_list = rng.randint(1, np.iinfo(np.int32).max, runs)
 
 
-def single_run(n_subjects, n_clusters, rho, sparsity, fdr, n_bootstraps, seed=None):
-    # Generate data
-    X, y, _, non_zero_index = simu_data(
-        n_subjects, n_clusters, rho=rho, sparsity=sparsity, seed=seed, snr=snr
-    )
+def different_result(n_subjects):
+    # Number of variables
+    n_clusters = 100
+    # Correlation parameter
+    rho = 0.3
+    # Ratio of number of variables with non-zero coefficients over total
+    # coefficients
+    sparsity = 0.1
+    # Desired controlled False Discovery Rate (FDR) level
+    fdr = 0.1
+    snr = 10
+    seed = 45
+    n_bootstraps = 25
+    runs = 20
+    n_jobs = None
+    joblib_verbose = 0
 
-    # Use model-X Knockoffs [1]
-    selected, test_scores, threshold, X_tildes = model_x_knockoff(
-        X,
-        y,
-        estimator=LassoCV(
-            n_jobs=1,
-            cv=KFold(n_splits=5, shuffle=True, random_state=0),
-        ),
-        n_bootstraps=1,
-        random_state=seed,
-    )
-    mx_selection, _ = model_x_knockoff_pvalue(test_scores, fdr=fdr)
-    fdp_mx, power_mx = cal_fdp_power(mx_selection, non_zero_index)
+    rng = check_random_state(seed)
+    seed_list = rng.randint(1, np.iinfo(np.int32).max, runs)
 
-    # Use p-values aggregation [2]
-    selected, test_scores, threshold, X_tildes = model_x_knockoff(
-        X,
-        y,
-        estimator=LassoCV(
-            n_jobs=1,
-            cv=KFold(n_splits=5, shuffle=True, random_state=0),
-        ),
-        n_bootstraps=n_bootstraps,
-        n_jobs=1,
-        random_state=seed,
-    )
-    aggregated_ko_selection, _, _ = model_x_knockoff_bootstrap_quantile(
-        test_scores, fdr=fdr, gamma=0.3
-    )
-
-    fdp_pval, power_pval = cal_fdp_power(aggregated_ko_selection, non_zero_index)
-
-    # Use e-values aggregation [1]
-    eval_selection, _, _ = model_x_knockoff_bootstrap_e_value(
-        test_scores, threshold, fdr=fdr
-    )
-
-    fdp_eval, power_eval = cal_fdp_power(eval_selection, non_zero_index)
-
-    return fdp_mx, fdp_pval, fdp_eval, power_mx, power_pval, power_eval
-
-
-parallel = Parallel(n_jobs, verbose=joblib_verbose)
-results = parallel(
-    delayed(single_run)(
-        n_subjects, n_clusters, rho, sparsity, fdr, n_bootstraps, seed=seed
-    )
-    for seed in seed_list
-)
-
-fdps_mx = []
-fdps_pval = []
-fdps_eval = []
-powers_mx = []
-powers_pval = []
-powers_eval = []
-for result in results:
-    fdp_mx, fdp_pval, fdp_eval, power_mx, power_pval, power_eval = result
-    fdps_mx.append(fdp_mx)
-    fdps_pval.append(fdp_pval)
-    fdps_eval.append(fdp_eval)
-
-    powers_mx.append(fdp_mx)
-    powers_pval.append(power_pval)
-    powers_eval.append(power_eval)
-
-# Plot FDP and Power distributions
-
-fdps = [fdps_mx, fdps_pval, fdps_eval]
-powers = [powers_mx, powers_pval, powers_eval]
-
-
-def plot_results(bounds, fdr, nsubjects, n_clusters, rho, power=False):
-    plt.figure(figsize=(5, 5), layout="constrained")
-    for nb in range(len(bounds)):
-        for i in range(len(bounds[nb])):
-            y = bounds[nb][i]
-            x = np.random.normal(nb + 1, 0.05)
-            plt.scatter(x, y, alpha=0.65, c="blue")
-
-    plt.boxplot(bounds, sym="")
-    if power:
-        plt.xticks(
-            [1, 2, 3],
-            ["MX Knockoffs", "Quantile aggregation", "e-values aggregation"],
-            rotation=45,
-            ha="right",
+    def single_run(n_subjects, n_clusters, rho, sparsity, fdr, n_bootstraps, seed=None):
+        # Generate data
+        X, y, _, non_zero_index = simu_data(
+            n_subjects, n_clusters, rho=rho, sparsity=sparsity, seed=seed, snr=snr
         )
-        plt.title(f"FDR = {fdr}, n = {nsubjects}, p = {n_clusters}, rho = {rho}")
-        plt.ylabel("Empirical Power")
 
-    else:
-        plt.hlines(fdr, xmin=0.5, xmax=3.5, label="Requested FDR control", color="red")
-        plt.xticks(
-            [1, 2, 3],
-            ["MX Knockoffs", "Quantile aggregation", "e-values aggregation"],
-            rotation=45,
-            ha="right",
+        # Use model-X Knockoffs [1]
+        selected, test_scores, threshold, X_tildes = model_x_knockoff(
+            X,
+            y,
+            estimator=LassoCV(
+                n_jobs=1,
+                cv=KFold(n_splits=5, shuffle=True, random_state=0),
+            ),
+            n_bootstraps=1,
+            random_state=seed,
         )
-        plt.title(f"FDR = {fdr}, n = {nsubjects}, p = {n_clusters}, rho = {rho}")
-        plt.ylabel("Empirical FDP")
-        plt.legend(loc="best")
+        mx_selection, _ = model_x_knockoff_pvalue(test_scores, fdr=fdr)
+        fdp_mx, power_mx = cal_fdp_power(mx_selection, non_zero_index)
 
-    plt.show()
+        # Use p-values aggregation [2]
+        selected, test_scores, threshold, X_tildes = model_x_knockoff(
+            X,
+            y,
+            estimator=LassoCV(
+                n_jobs=1,
+                cv=KFold(n_splits=5, shuffle=True, random_state=0),
+            ),
+            n_bootstraps=n_bootstraps,
+            n_jobs=1,
+            random_state=seed,
+        )
+        aggregated_ko_selection, _, _ = model_x_knockoff_bootstrap_quantile(
+            test_scores, fdr=fdr, gamma=0.3
+        )
+
+        fdp_pval, power_pval = cal_fdp_power(aggregated_ko_selection, non_zero_index)
+
+        # Use e-values aggregation [1]
+        eval_selection, _, _ = model_x_knockoff_bootstrap_e_value(
+            test_scores, threshold, fdr=fdr
+        )
+
+        fdp_eval, power_eval = cal_fdp_power(eval_selection, non_zero_index)
+
+        return fdp_mx, fdp_pval, fdp_eval, power_mx, power_pval, power_eval
+
+    parallel = Parallel(n_jobs, verbose=joblib_verbose)
+    results = parallel(
+        delayed(single_run)(
+            n_subjects, n_clusters, rho, sparsity, fdr, n_bootstraps, seed=seed
+        )
+        for seed in seed_list
+    )
+
+    fdps_mx = []
+    fdps_pval = []
+    fdps_eval = []
+    powers_mx = []
+    powers_pval = []
+    powers_eval = []
+    for result in results:
+        fdp_mx, fdp_pval, fdp_eval, power_mx, power_pval, power_eval = result
+        fdps_mx.append(fdp_mx)
+        fdps_pval.append(fdp_pval)
+        fdps_eval.append(fdp_eval)
+
+        powers_mx.append(fdp_mx)
+        powers_pval.append(power_pval)
+        powers_eval.append(power_eval)
+
+    # Plot FDP and Power distributions
+
+    fdps = [fdps_mx, fdps_pval, fdps_eval]
+    powers = [powers_mx, powers_pval, powers_eval]
+
+    def plot_results(bounds, fdr, nsubjects, n_clusters, rho, power=False):
+        plt.figure(figsize=(5, 5), layout="constrained")
+        for nb in range(len(bounds)):
+            for i in range(len(bounds[nb])):
+                y = bounds[nb][i]
+                x = np.random.normal(nb + 1, 0.05)
+                plt.scatter(x, y, alpha=0.65, c="blue")
+
+        plt.boxplot(bounds, sym="")
+        if power:
+            plt.xticks(
+                [1, 2, 3],
+                ["MX Knockoffs", "Quantile aggregation", "e-values aggregation"],
+                rotation=45,
+                ha="right",
+            )
+            plt.title(f"FDR = {fdr}, n = {nsubjects}, p = {n_clusters}, rho = {rho}")
+            plt.ylabel("Empirical Power")
+
+        else:
+            plt.hlines(
+                fdr, xmin=0.5, xmax=3.5, label="Requested FDR control", color="red"
+            )
+            plt.xticks(
+                [1, 2, 3],
+                ["MX Knockoffs", "Quantile aggregation", "e-values aggregation"],
+                rotation=45,
+                ha="right",
+            )
+            plt.title(f"FDR = {fdr}, n = {nsubjects}, p = {n_clusters}, rho = {rho}")
+            plt.ylabel("Empirical FDP")
+            plt.legend(loc="best")
+
+        plt.show()
+
+    plot_results(fdps, fdr, n_subjects, n_clusters, rho)
+    plot_results(powers, fdr, n_subjects, n_clusters, rho, power=True)
 
 
-plot_results(fdps, fdr, n_subjects, n_clusters, rho)
-plot_results(powers, fdr, n_subjects, n_clusters, rho, power=True)
+different_result(n_subjects=100)
+different_result(n_subjects=50)
