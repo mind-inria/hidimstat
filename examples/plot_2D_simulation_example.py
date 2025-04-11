91e669d4e5b4a514d18b4713cd993547ee93edc7
kusch lionel <lionel.a.kusch@inria.fr>
2025-04-11 10:34:40 +0200

    Improve the example

--- examples/plot_2D_simulation_example.py
+++ examples/plot_2D_simulation_example.py
@@ -73,8 +73,11 @@ from hidimstat.desparsified_lasso import (
     desparsified_lasso,
     desparsified_lasso_pvalue,
 )
+from hidimstat.empirical_thresholding import (
+    empirical_thresholding
+)
 from hidimstat.scenario import multivariate_simulation
-from hidimstat.stat_tools import zscore_from_pval
+from hidimstat.stat_tools import zscore_from_pval, pval_from_scale
 
 #############################################################################
 # Specific plotting functions
@@ -239,6 +242,25 @@ thr_nc = zscore_from_pval((fwer_target / 2) * correction_no_cluster)
 beta_extended = weight_map_2D_extended(shape, roi_size, delta)
 
 #############################################################################
+# Inference with several algorithms
+# ---------------------------------
+#
+# The most naive way to find the true support is to use a threshold
+# using a linear estimator
+
+beta_hat, scale = empirical_thresholding(X_init, y)
+pval, pval_corr, one_minus_pval, one_minus_pval_corr = pval_from_scale(beta_hat, scale)
+
+# compute estimated support (first method)
+zscore = zscore_from_pval(pval, one_minus_pval)
+selected_emp_th = zscore > thr_nc  # use the "no clustering threshold"
+
+# compute estimated support (second method)
+selected_emp_th = np.logical_or(
+    pval_corr < fwer_target / 2, one_minus_pval_corr < fwer_target / 2
+)
+
+#############################################################################
 # Now, we compute the support estimated by a high-dimensional statistical
 # infernece method that does not leverage the data structure. This method
 # was introduced by Javanmard, A. et al. (2014), Zhang, C. H. et al. (2014)
@@ -339,11 +361,11 @@ titles.append("True weights")
 maps.append(np.reshape(beta_extended, shape))
 titles.append("True weights \nwith tolerance")
 
+maps.append(np.reshape(selected_emp_th, shape))
+titles.append("Empirical Thresholding")
+
 maps.append(np.reshape(selected_dl, shape))
 titles.append("Desparsified Lasso")
-
-maps.append(None)
-titles.append(None)
 
 maps.append(np.reshape(selected_cdl, shape))
 titles.append("CluDL")
