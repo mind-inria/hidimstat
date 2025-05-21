fce4be545ce8808955fd6013d8636794dc7c8b77
lionel kusch <lionel.a.kusch@inria.fr>
2025-05-21 15:13:50 +0200

    Improvement of circle ci (#261)
    * remove conda
    
    * add comments and fix some bugs
    
    * add comments
    
    * update the circle ci artifact
    
    * fix bug
    
    * improve the push of documentation
    
    * improve circleci
    
    * create a file for setup the virtual environement
    
    * move the file
    
    * change permisson of the file
    
    * test push
    
    * remove depreciate circleci
    
    * test deploy
    
    * remove tests for deploy
    
    * Update the option for building the documentation
    
    * Add a document to explain the actual "tag" in the commits
    
    * Fix link in the changed file
    
    * Update build_tools/circle/push_doc.sh
    
    ---------
    
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>

---
+++ build_tools/CI_documentation.rst
@@ -0,0 +1,26 @@
+Continuous Integration (CI)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+After creating your PR, CI tools will run proceed to run all the tests on all
+configurations supported by hidimstat.
+
+- **Github Actions**:
+  Used for testing hidimstat across various platforms (Linux, macOS, Windows).
+- **CircleCI**:
+  Builds and verifies the project documentation.
+
+If any of the following markers appears in the commit message, the following
+actions are taken.
+
+    ====================== ===================
+    Commit Message Marker  Action Taken by CI
+    ---------------------- -------------------
+    [doc skip]             Docs are not built
+    [doc quick]            Docs built, but excludes example gallery plots
+    [doc change]           Docs built including only example which has been directly modified
+    ====================== ===================
+
+Note that by default the documentation is built with all the example are modified.
+
+CI is testing all possible configurations supported by hidimstat, so tests may fail
+with configurations different from what you are developing with. See with the 
+mainteners to identify the cause of this failure. 
--- build_tools/circle/build_doc.sh
+++ build_tools/circle/build_doc.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+# display all the command and associet argument when they are executed
 set -x
+# exit immediately if a command exits with a non-zero status.
 set -e
+
+# activate the environment for the creation of the documentation
+# see the file setup_virtual_environment
+source .venv/bin/activate
 
 # Decide what kind of documentation build to run, and run it.
 #
@@ -18,11 +25,19 @@ set -e
 # behavior is to quick build the documentation.
 
 get_build_type() {
+    # Full build if it is not in a PR
+    if [ -z "$CI_PULL_REQUEST" ]
+    then
+        echo BUILD: not a pull request
+        return
+    fi
+    # get the hash of the last commit of the PR
     if [ -z "$CIRCLE_SHA1" ]
     then
         echo SKIP: undefined CIRCLE_SHA1
         return
     fi
+    # get the log of commit and detect marker: [doc skip], [doc quick], [doc changed]
     commit_msg=$(git log --format=%B -n 1 $CIRCLE_SHA1)
     if [ -z "$commit_msg" ]
     then
@@ -39,50 +54,52 @@ get_build_type() {
         echo QUICK: [doc quick] marker found
         return
     fi
-    if [[ "$commit_msg" =~ \[doc\ build\] ]]
+    if [[ "$commit_msg" =~ \[doc\ change\] ]]
     then
-        echo BUILD: [doc build] marker found
+        # get the example difference between main and the actual commit
+        git_range="origin/main...$CIRCLE_SHA1"
+        git fetch origin main >&2 || (echo QUICK BUILD: failed to get changed filenames for $git_range; return)
+        filenames=$(git diff --name-only $git_range)
+        # case where there is no modifed file
+        if [ -z "$filenames" ]
+        then
+            echo QUICK BUILD: no changed filenames for $git_range
+            return
+        fi
+        # get the examples which have been modified
+        changed_examples=$(echo "$filenames" | grep -e ^examples/)
+        if [[ -n "$changed_examples" ]]
+        then
+            echo BUILD: detected examples/ filename modified in $git_range: $changed_examples
+            pattern=$(echo "$changed_examples" | paste -sd '|')
+            # pattern for examples to run is the last line of output
+            echo "$pattern"
+            return
+        fi
+        # case where there is no modified example 
+        echo QUICK BUILD: no examples/ filename modified in $git_range:
+        echo "$filenames"
         return
     fi
-    if [ -z "$CI_PULL_REQUEST" ]
-    then
-        echo BUILD: not a pull request
-        return
-    fi
-    git_range="origin/main...$CIRCLE_SHA1"
-    git fetch origin main >&2 || (echo QUICK BUILD: failed to get changed filenames for $git_range; return)
-    filenames=$(git diff --name-only $git_range)
-    if [ -z "$filenames" ]
-    then
-        echo QUICK BUILD: no changed filenames for $git_range
-        return
-    fi
-    changed_examples=$(echo "$filenames" | grep -e ^examples/)
-    if [[ -n "$changed_examples" ]]
-    then
-        echo BUILD: detected examples/ filename modified in $git_range: $changed_examples
-        pattern=$(echo "$changed_examples" | paste -sd '|')
-        # pattern for examples to run is the last line of output
-        echo "$pattern"
-        return
-    fi
-    echo QUICK BUILD: no examples/ filename modified in $git_range:
-    echo "$filenames"
+    echo BUILD: build all the example by default
 }
 
 build_type=$(get_build_type)
+# Skip examples
 if [[ "$build_type" =~ ^SKIP ]]
 then
     exit 0
 fi
-
+# generate all the example if it's push on main or on a previous version
 if [[ "$CIRCLE_BRANCH" =~ ^main$|^[0-9]+\.[0-9]+\.X$ && -z "$CI_PULL_REQUEST" ]]
 then
     make_args="html"
 elif [[ "$build_type" =~ ^QUICK ]]
+# do not generate examples
 then
     make_args="html-noplot"
 elif [[ "$build_type" =~ ^'BUILD: detected examples' ]]
+# generate only example wich has been modified
 then
     # pattern for examples to run is the last line of output
     pattern=$(echo "$build_type" | tail -n 1)
@@ -91,47 +108,29 @@ else
     make_args="html"
 fi
 
-# deactivate circleci virtualenv and setup a miniconda env instead
-if [[ `type -t deactivate` ]]; then
-    deactivate
-fi
-
-# Install dependencies with miniconda
-wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh" -O miniconda.sh
-chmod +x miniconda.sh && bash ./miniconda.sh -b -p "miniconda"
-export PATH="miniconda/bin:$PATH"
-
-# Configure the conda environment and put it in the path using the
-# provided versions
-mamba create -n $CONDA_ENV_NAME --yes python="${PYTHON_VERSION:-*}" \
-      numpy="${NUMPY_VERSION:-*}" scipy="${SCIPY_VERSION:-*}" \
-      pytest coverage matplotlib="${MATPLOTLIB_VERSION:-*}" sphinx \
-      seaborn statsmodels pillow cython joblib pandas="${PANDAS_VERSION:-*}"
-
-source activate $CONDA_ENV_NAME
-
-pip install -e ".[doc]"
-
-
 # The pipefail is requested to propagate exit code
-set -o pipefail && cd doc_conf && make $make_args 2>&1 | tee ~/output_sphinx.log
-
+set -o pipefail && cd doc_conf && make $make_args 2>&1 | tee ~/log.txt
 cd -
 
 set +o pipefail
 
 
 affected_doc_paths() {
+    # generate a list fo the file modified in the PR
     files=$(git diff --name-only origin/main...$CIRCLE_SHA1)
+    # list of the modfied documentation files
     echo "$files" | grep ^doc_conf/.*\.rst | sed 's/^doc_conf\/\(.*\)\.rst$/\1.html/'
+    # list of the modified examples 
     echo "$files" | grep ^examples/.*.py | sed 's/^\(.*\)\.py$/auto_\1.html/'
-    project_files=$(echo "$files" | grep '^hidimstat/')
+    # list of the modifed source file
+    project_files=$(echo "$files" | grep 'src/hidimstat/')
     if [ -n "$project_files" ]
     then
-        grep -hlR -f<(echo "$project_files" | sed 's/^/hidimstat\/blob\/[a-z0-9]*\//') doc_conf/_build/html/modules/generated | cut -d/ -f5-
+        grep -hlR -f<(echo "$project_files" | sed 's/src\/hidimstat\//hidimstat\./') doc_conf/_build/html/generated | cut -d/ -f4- 
     fi
 }
 
+# generate a html page which list the modified files
 if [ -n "$CI_PULL_REQUEST" ]
 then
     echo "The following documentation files may have been changed by PR #$CI_PULL_REQUEST:"
@@ -140,6 +139,6 @@ then
     (
     echo '<html><body><ul>'
     echo "$affected" | sed 's|.*|<li><a href="&">&</a></li>|'
-    echo '</ul><p>General: <a href="index.html">Home</a> | <a href="modules/classes.html">API Reference</a> | <a href="auto_examples/index.html">Examples</a></p></body></html>'
+    echo '</ul><p>General: <a href="index.html">Home</a> | <a href="api.html">API Reference</a> | <a href="auto_examples/index.html">Examples</a></p></body></html>'
     ) > 'doc_conf/_build/html/_changed.html'
 fi
