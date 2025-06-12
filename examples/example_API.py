6d428c28676fb048f161718f3e188f36c17a762d
kusch lionel <lionel.a.kusch@inria.fr>
2025-06-12 17:31:27 +0200

    add the API version 3

--- examples/example_API.py
+++ examples/example_API.py
@@ -16,10 +16,11 @@ from sklearn.model_selection import KFold
 rng = np.random.RandomState(0)
 X, y = make_circles(n_samples=500, noise=0.1, factor=0.6, random_state=rng)
 
-###############################################################################
-# 1:API post sprint
-# ==================
-from hidimstat_mock.v1 import PFI, LOCO, D0CRT, Knockoff
+#############################################################################
+# 3:API 11/06 
+# ================
+from hidimstat_mock.v3 import PFI, LOCO, D0CRT, Knockoff
+cv = KFold(n_splits=5, shuffle=True, random_state=0)
 ###############################################################################
 # Variable Importance:
 # --------------------
@@ -27,41 +28,36 @@ from hidimstat_mock.v1 import PFI, LOCO, D0CRT, Knockoff
 
 vi = PFI(estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X, y))
 vi.fit(X, y)
-importances_vi = vi.importance() # vector of n_features 
-selection_vi = vi.selection(10) # binary vector of n_features
+importances_vi = vi.importance(None, None) # vector of n_features 
+importances_vi, None = vi.importance_fit(X, y, cv) # vector of n_features, not p-value 
+selection_vi = selection(importances_vi, None, percentage=0.1) # binary vector of n_features
 ###############################################################################
 # Controlled Variable Importance:
 # -------------------------------
 # Variable importance, p-value and selection using LOCO
-cv = KFold(n_splits=5, shuffle=True, random_state=0)
-importances_cvi = []
-selection_cvi = []
-for train, test in cv.split(X):
-    cvi = LOCO(
-        estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X[train], y[train])
-        )
-    cvi.fit(X[train], y[train]) 
-    importances = cvi.importance(X[test], y[test]) # vector of n_features 
-    importances_cvi.append(importances)
-    selection = cvi.selection(10) # binary vector of n_features
-    selection_cvi.append(selection)
-_, pval_cvi = ttest_1samp(importances_cvi, 0, axis=0, alternative="greater")
+cvi.fit(X[train], y[train]) 
+importances_cvi = cvi.importance(X[test], y[test]) # vector of n_features 
+importances_cvi, pvalue_cvi = cvi.importance(X, y, cv) # vector of n_features 
+selection_cvi = selection(importance_cvi, pvalue_cvi, threshold=2.0) # binary vector of n_features
 ###############################################################################
 # Individual Variable Selection:
 # ------------------------------
 # P-values and selection using d0CRT
 ivs = D0CRT()
 ivs.fit(X, y)
-pvalue_ivs = ivs.importance() # vector of n_features 
-selection_vi = ivs.selection(0.1) # binary vector of n_features
+# In this case importance and p-value can be very similar
+importances_ivs = ivs.importance(None, None) # vector of n_features 
+importance_ivs, pvalue_ivs = ivs.importance(X, y, None) # vector of n_features 
+selection_ivs = selection(importance_ivs, pvalue_ivs, k_best=20.0) # binary vector of n_features
 ###############################################################################
 # Multiple Variable Selection:
 # ----------------------------
 # selection using Knockoff
 mvs = Knockoff()
 mvs.fit(X, y)
-score_mvs = mvs.importance() # vector of n_features 
-selection_mvs = mvs.selection(FDP=0.1) # binary vector of n_features
+importance_mvs = mvs.importance(None, None) # vector of n_features 
+importance_mvs, pvalue_mvs = mvs.importance(X, y, None) # vector of n_features 
+selection_mvs = selection(importance_mvs, pvalue_mvs, k_best=20.0, threshold_pvalue=0.8) # binary vector of n_features
 
 #############################################################################
 # 2:API 26 Mai 
@@ -107,3 +103,50 @@ mvs.fit(X, y)
 # REMOVED: score_mvs = mvs.importance(None, None) # vector of n_features 
 selection_mvs, ?_? = mvs.selection(None, None, FDP=0.1) # binary vector of n_features
 # WARNING: knoclkoff doesn't have p-values?
+
+###############################################################################
+# 1:API post sprint  4/4/2025
+# ===========================
+from hidimstat_mock.v1 import PFI, LOCO, D0CRT, Knockoff
+###############################################################################
+# Variable Importance:
+# --------------------
+# Variable importance and selection using Permutation Features Importance
+
+vi = PFI(estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X, y))
+vi.fit(X, y)
+importances_vi = vi.importance() # vector of n_features 
+selection_vi = vi.selection(10) # binary vector of n_features
+###############################################################################
+# Controlled Variable Importance:
+# -------------------------------
+# Variable importance, p-value and selection using LOCO
+cv = KFold(n_splits=5, shuffle=True, random_state=0)
+importances_cvi = []
+selection_cvi = []
+for train, test in cv.split(X):
+    cvi = LOCO(
+        estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X[train], y[train])
+        )
+    cvi.fit(X[train], y[train]) 
+    importances = cvi.importance(X[test], y[test]) # vector of n_features 
+    importances_cvi.append(importances)
+    selection = cvi.selection(10) # binary vector of n_features
+    selection_cvi.append(selection)
+_, pval_cvi = ttest_1samp(importances_cvi, 0, axis=0, alternative="greater")
+###############################################################################
+# Individual Variable Selection:
+# ------------------------------
+# P-values and selection using d0CRT
+ivs = D0CRT()
+ivs.fit(X, y)
+pvalue_ivs = ivs.importance() # vector of n_features 
+selection_vi = ivs.selection(0.1) # binary vector of n_features
+###############################################################################
+# Multiple Variable Selection:
+# ----------------------------
+# selection using Knockoff
+mvs = Knockoff()
+mvs.fit(X, y)
+score_mvs = mvs.importance() # vector of n_features 
+selection_mvs = mvs.selection(FDP=0.1) # binary vector of n_features
