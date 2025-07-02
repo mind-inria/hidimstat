492c9f4b601a7b6e2ab483a5463970b9f5b795ae
kusch lionel <lionel.a.kusch@inria.fr>
2025-07-02 16:37:34 +0200

    update API

--- examples/example_API.py
+++ examples/example_API.py
@@ -17,6 +17,48 @@ rng = np.random.RandomState(0)
 X, y = make_circles(n_samples=500, noise=0.1, factor=0.6, random_state=rng)
 
 #############################################################################
+# 4:API O2/07 
+# ================
+from hidimstat_mock.v4 import PFI, LOCO, D0CRT, Knockoff
+cv = KFold(n_splits=5, shuffle=True, random_state=0)
+###############################################################################
+# Variable Importance:
+# --------------------
+# Variable importance and selection using Permutation Features Importance
+
+vi = PFI(estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X, y))
+vi.fit(X, y)
+importances_vi = vi.importance(None, None) # vector of n_features 
+importances_vi, None = vi.importance_fit(X, y, cv) # vector of n_features, not p-value 
+selection_vi = vi.selection(percentage=0.1, threshold=None, k_best=None, threshold_pvalue=None) # binary vector of n_features
+###############################################################################
+# Controlled Variable Importance:
+# -------------------------------
+# Variable importance, p-value and selection using LOCO
+cvi = LOCO(estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X, y))
+cvi.fit(X[train], y[train]) 
+importances_cvi = cvi.importance(X[test], y[test]) # vector of n_features 
+importances_cvi, pvalue_cvi = cvi.importance(X, y, cv) # vector of n_features 
+selection_cvi = cvi.selection(percentage=None, threshold=2.0, k_best=None, threshold_pvalue=None) # binary vector of n_features
+###############################################################################
+# Variable Selection:
+# ------------------------------
+# P-values and selection using d0CRT
+vs = D0CRT()
+vs.fit(X, y)
+# In this case importance and p-value can be very similar
+importances_vs = vs.importance(None, None) # vector of n_features 
+importance_vs, pvalue_vs = vs.importance(X, y, None) # vector of n_features 
+selection_vs = vs.selection(percentage=None, threshold=None, k_best=20.0, threshold_pvalue=None) # binary vector of n_features
+# ----------------------------
+# selection using Knockoff
+vs = Knockoff()
+vs.fit(X, y)
+importance_vs = vs.importance(None, None) # vector of n_features 
+importance_vs, pvalue_vs = vs.importance(X, y, None) # vector of n_features 
+selection_vs = vs.selection(percentage=None, threshold=None, k_best=20.0, threshold_pvalue=0.8) # binary vector of n_features
+
+#############################################################################
 # 3:API 11/06 
 # ================
 from hidimstat_mock.v3 import PFI, LOCO, D0CRT, Knockoff
@@ -35,10 +77,11 @@ selection_vi = selection(importances_vi, None, percentage=0.1) # binary vector o
 # Controlled Variable Importance:
 # -------------------------------
 # Variable importance, p-value and selection using LOCO
+cvi = LOCO(estimator=LogisticRegressionCV(Cs=np.logspace(-3, 3, 5)).fit(X, y))
 cvi.fit(X[train], y[train]) 
 importances_cvi = cvi.importance(X[test], y[test]) # vector of n_features 
 importances_cvi, pvalue_cvi = cvi.importance(X, y, cv) # vector of n_features 
-selection_cvi = selection(importance_cvi, pvalue_cvi, threshold=2.0) # binary vector of n_features
+selection_cvi = selection(importances_cvi, pvalue_cvi, threshold=2.0) # binary vector of n_features
 ###############################################################################
 # Individual Variable Selection:
 # ------------------------------
