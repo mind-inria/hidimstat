f79f9701317dc42669853c423328ee4a4b82b2b7
kusch lionel <lionel.a.kusch@inria.fr>
2025-07-04 16:43:26 +0200

    Create an abstract classes

---
+++ src/hidimstat/base_variable_importance.py
@@ -0,0 +1,110 @@
+import warnings
+
+from sklearn.base import BaseEstimator
+import numpy as np
+
+
+class BaseVariableImportance(BaseEstimator):
+    """
+    Class of the base for methods of variable of importance
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.importances_ = None
+        self.pvalues_ = None
+
+    def selection(
+        self, k_best=None, percentile=None, threshold=None, threshold_pvalue=None
+    ):
+        """
+        Selection of the variable importance
+
+        Parameters
+        ----------
+        k_best : int, optional, default=None
+            Selection of the k best of features
+        percentile : float, optional, default=None
+            Selection of percentile of features
+        threshold : _type_, optional, default=None
+            Selection of the features which has higher importance that the threshold
+        threshold_pvalue : _type_, optional, default=None
+            Selection of the features which has higher pvalue that the threshold
+
+        Returns
+        -------
+        selection: binary array-like of shape (n_features)
+            Binary array of the seleted features
+        """
+        self._check_importance()
+        if k_best is not None:
+            if not isinstance(k_best, str) and k_best > self.importances_.shape[1]:
+                warnings.warn(
+                    f"k={k_best} is greater than n_features={self.importances_.shape[1]}. "
+                    "All the features will be returned."
+                )
+            assert k_best > 0, "k_best needs to be positive and not null"
+        if percentile is not None:
+            assert (
+                0 < percentile and percentile < 100
+            ), "percentille needs to be between 0 and 100"
+        if threshold_pvalue is not None:
+            assert (
+                0 < threshold_pvalue and threshold_pvalue > 100
+            ), "threshold needs to be between 0 and 1"
+
+        # base on SelectKBest of Scikit-Learn
+        if k_best == "all":
+            mask_k_best = np.ones(self.importances_.shape, dtype=bool)
+        elif k_best == 0:
+            mask_k_best = np.zeros(self.importances_.shape, dtype=bool)
+        elif k_best is not None:
+            mask_k_best = np.zeros(self.importances_.shape, dtype=bool)
+
+            # Request a stable sort. Mergesort takes more memory (~40MB per
+            # megafeature on x86-64).
+            mask_k_best[np.argsort(self.importances_, kind="mergesort")[-k_best:]] = 1
+        else:
+            mask_k_best = np.ones(self.importances_.shape, dtype=bool)
+
+        # base on SelectPercentile of Scikit-Learn
+        if percentile == 100:
+            mask_percentile = np.ones(len(self.importances_), dtype=bool)
+        elif percentile == 0:
+            mask_percentile = np.zeros(len(self.importances_), dtype=bool)
+        elif percentile is not None:
+            threshold = np.percentile(self.importances_, 100 - percentile)
+            mask_percentile = self.importances_ > threshold
+            ties = np.where(self.importances_ == threshold)[0]
+            if len(ties):
+                max_feats = int(len(self.importances_) * percentile / 100)
+                kept_ties = ties[: max_feats - mask_percentile.sum()]
+                mask_percentile[kept_ties] = True
+        else:
+            mask_percentile = np.ones(self.importances_.shape, dtype=bool)
+
+        if threshold is not None:
+            mask_threshold = self.importances_ < threshold
+        else:
+            mask_threshold = np.ones(self.importances_.shape, dtype=bool)
+
+        # base on SelectFpr of Scikit-Learn
+        if threshold_pvalue is not None:
+            mask_threshold_pvalue = self.pvalues_ < threshold_pvalue
+        else:
+            mask_threshold_pvalue = np.ones(self.importances_.shape, dtype=bool)
+
+        selection = (
+            mask_k_best & mask_percentile & mask_threshold & mask_threshold_pvalue
+        )
+
+        return selection
+
+    def _check_importance(self):
+        """
+        Check if the importance was computed
+        """
+        if self.importances_ is not None or self.pvalues_ is not None:
+            raise ValueError(
+                "The importances need to be called before to call this method"
+            )
