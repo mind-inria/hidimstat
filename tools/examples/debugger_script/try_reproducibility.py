93fd34fcdcaf9cd9f0da87f261259f377c9f6d63
kusch lionel <lionel.a.kusch@inria.fr>
2025-08-22 18:28:59 +0200

    Test for testing reproducibilities

---
+++ tools/examples/debugger_script/try_reproducibility.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+from joblib import Parallel, delayed
+
+# add the example for import them
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../examples")
+
+
+def run_joblib(i):
+    import plot_knockoff_aggregation  # include the example to test
+
+
+# run in parralel the same example for compare result
+parrallel = Parallel(n_jobs=4)
+parrallel(delayed(run_joblib)(i) for i in range(6))
