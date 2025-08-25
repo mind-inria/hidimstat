0b90779f813721c0bed60ad41b0493138d1dff6b
kusch lionel <lionel.a.kusch@inria.fr>
2025-08-25 18:32:48 +0200

    fix path

--- tools/examples/debugger_script/try_reproducibility.py
+++ tools/examples/debugger_script/try_reproducibility.py
@@ -5,7 +5,7 @@ import sys
 from joblib import Parallel, delayed
 
 # add the example for import them
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../examples")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../examples")
 
 
 def run_joblib(i):
