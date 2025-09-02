82d61e6120eb55c83a280c3e4944e6ab1ef311f6
kusch lionel <lionel.a.kusch@inria.fr>
2025-09-02 17:23:02 +0200

    add test for new check

--- test/test_base_perturbation.py
+++ test/test_base_perturbation.py
@@ -12,3 +12,16 @@ def test_no_implemented_methods():
     basic_class = BasePerturbation(estimator=estimator)
     with pytest.raises(NotImplementedError):
         basic_class._permutation(X, group_id=None)
+
+
+def test_chek_importance():
+    """test that the methods are not implemented in the base class"""
+    X = np.random.randint(0, 2, size=(100, 2, 1))
+    estimator = LinearRegression()
+    estimator.fit(X[:, 0], X[:, 1])
+    basic_class = BasePerturbation(estimator=estimator)
+    basic_class.importances_ = []
+    with pytest.raises(
+        ValueError, match="The importances need to be called before calling this method"
+    ):
+        basic_class.selection()
