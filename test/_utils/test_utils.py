e44bab4e10b9327216d46462ed2c970785699bf8
kusch lionel <lionel.a.kusch@inria.fr>
2025-09-10 17:58:04 +0200

    add test for new function

---
+++ test/_utils/test_utils.py
@@ -0,0 +1,78 @@
+import numbers
+import numpy as np
+from hidimstat._utils.utils import (
+    get_seed_generator,
+    check_random_state,
+    NoneGenerator,
+    SeedGenerator,
+    SequenceGenerator,
+)
+
+
+def test_none():
+    "test random state is None"
+    random_state = None
+    check_random_state(random_state)
+    generator = get_seed_generator(random_state)
+
+    assert isinstance(generator, NoneGenerator)
+
+    seed = generator.get_seed(52)
+    assert seed is None
+
+
+def test_integer():
+    "test random state is integer"
+    random_state = 10
+    check_random_state(random_state)
+    generator = get_seed_generator(random_state)
+
+    assert isinstance(generator, SeedGenerator)
+
+    seed = generator.get_seed(52)
+    assert isinstance(seed, list)
+    assert len(seed) == 2
+    assert seed[0] == 52
+    assert isinstance(seed[1], numbers.Integral)
+
+
+def test_tuple_2_integer():
+    "test random state is a tuple of 2 elements"
+    random_state = (0, 1)
+    check_random_state(random_state)
+    generator = get_seed_generator(random_state)
+
+    assert isinstance(generator, SeedGenerator)
+
+    seed = generator.get_seed(52)
+    assert isinstance(seed, list)
+    assert len(seed) == 2
+    assert seed[0] == 52
+    assert isinstance(seed[1], numbers.Integral)
+
+
+def test_array_2_integer():
+    "test random state is an array of 2 elements"
+    random_state = [0, 1]
+    check_random_state(random_state)
+    generator = get_seed_generator(random_state)
+
+    assert isinstance(generator, SeedGenerator)
+
+    seed = generator.get_seed(52)
+    assert isinstance(seed, list)
+    assert len(seed) == 2
+    assert seed[0] == 52
+    assert isinstance(seed[1], numbers.Integral)
+
+
+def test_rng():
+    "test random state is rng"
+    random_state = np.random.RandomState(0)
+    check_random_state(random_state)
+    generator = get_seed_generator(random_state)
+
+    assert isinstance(generator, SequenceGenerator)
+
+    seed = generator.get_seed(52)
+    assert isinstance(seed, np.random.RandomState)
