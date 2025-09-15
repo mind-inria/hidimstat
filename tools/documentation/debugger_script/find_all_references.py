b9321bfcb28617fc91e8033bedc156cf16fb2b3b
kusch lionel <lionel.a.kusch@inria.fr>
2025-09-15 14:28:21 +0200

    add comments

--- tools/documentation/debugger_script/find_all_references.py
+++ tools/documentation/debugger_script/find_all_references.py
@@ -1,6 +1,10 @@
 import os
 import pickle
 
+# This is tool is based on: https://stackoverflow.com/questions/26666859/how-do-i-print-all-labels-that-are-defined-in-a-sphinx-project
+# It's for getting all the labels generated during the documentation.
+# It's useful when we check the reason for undefined label warnings.
+
 path_file = os.path.dirname(os.path.abspath(__file__))
 
 with open(path_file + "/../../../docs/_build/doctrees/environment.pickle", "rb") as f:
