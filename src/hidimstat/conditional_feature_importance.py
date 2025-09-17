f90e6bc43b1135ef3529e6b1aade8a823ca1993e
kusch lionel <lionel.a.kusch@inria.fr>
2025-09-17 17:45:43 +0200

    fix format

--- src/hidimstat/conditional_feature_importance.py
+++ src/hidimstat/conditional_feature_importance.py
@@ -4,8 +4,8 @@ from sklearn.base import BaseEstimator, check_is_fitted, clone
 from sklearn.metrics import root_mean_squared_error
 from sklearn.utils.validation import check_random_state
 
-from hidimstat.base_variable_importance import MixinSelectionFDR
 from hidimstat.base_perturbation import BasePerturbation
+from hidimstat.base_variable_importance import MixinSelectionFDR
 from hidimstat.conditional_sampling import ConditionalSampler
 
 
