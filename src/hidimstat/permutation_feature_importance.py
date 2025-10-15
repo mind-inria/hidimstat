5fc3405f1590e124b34923a0a7acf68168036d89
jpaillard <joseph.paillard@inria.fr>
2025-10-15 14:26:12 +0200

    test remove callable

--- src/hidimstat/permutation_feature_importance.py
+++ src/hidimstat/permutation_feature_importance.py
@@ -9,7 +9,7 @@ class PFI(BasePerturbation):
     def __init__(
         self,
         estimator,
-        loss: callable = root_mean_squared_error,
+        loss=root_mean_squared_error,
         method: str = "predict",
         n_permutations: int = 50,
         features_groups=None,
