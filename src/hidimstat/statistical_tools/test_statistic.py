759ef7dbce320b37946fa43a02b2af10d376ab35
kusch lionel <lionel.a.kusch@inria.fr>
2025-10-16 17:44:12 +0200

    modify scipy test with NB

--- src/hidimstat/statistical_tools/test_statistic.py
+++ src/hidimstat/statistical_tools/test_statistic.py
@@ -1,21 +1,31 @@
 import numpy as np
-from scipy import stats
-from scipy.stats._result_classes import TtestResult
+from scipy._lib._array_api import (
+    array_namespace,
+    xp_capabilities,
+)
+from scipy.stats._axis_nan_policy import _axis_nan_policy_factory
+from scipy.stats._stats_py import (
+    TtestResult,
+    _chk_asarray,
+    _get_nan,
+    _get_pvalue,
+    _length_nonmasked,
+    _SimpleStudentT,
+    _var,
+    pack_TtestResult,
+    unpack_TtestResult,
+)
 
 
-def corrected_std(differences, test_frac, axis=0):
+def _var_nadeau_bengio(differences, test_frac, axis=0, ddof=0, mean=None, xp=None):
     """
     Corrects standard deviation using Nadeau and Bengio's approach.
-    Copy from https://github.com/jpaillard/permucate/blob/3af6ab91edbdf40b62a66fe9a31e45c168050c3a/permucate/utils.py#L198-L218
+    Copy from https://github.com/scikit-learn/scikit-learn/blob/0c27a07f68e0eda7e1fcbce44a7615addec7f232/examples/model_selection/plot_grid_search_stats.py#L172
 
     Parameters
     ----------
     differences : ndarray of shape (n_samples,)
         Vector containing the differences in the score metrics of two models.
-    n_train : int
-        Number of samples in the training set.
-    n_test : int
-        Number of samples in the testing set.
 
     Returns
     -------
@@ -25,78 +35,83 @@ def corrected_std(differences, test_frac, axis=0):
     # kr = k times r, r times repeated k-fold crossvalidation,
     # kr equals the number of times the model was evaluated
     kr = differences.shape[axis]
-    corrected_var = np.var(differences, ddof=1, axis=axis) * (1 / kr + test_frac)
-    corrected_std = np.sqrt(corrected_var)
-    return corrected_std
+
+    var = _var(differences, ddof=ddof, axis=axis, mean=mean, xp=xp)
+    corrected_var = var * (1 / kr + test_frac)
+    return corrected_var
 
 
-def _ttest_finish(df, t, alternative):
-    """
-    Common code between all 3 t-test functions.
-    This is based on the following code:
-    https://github.com/scipy/scipy/blob/5e4a5e3785f79dd4e8930eed883da89958860db2/scipy/stats/_stats_py.py#L6972-6994
-    """
-    # We use ``stdtr`` directly here as it handles the case when ``nan``
-    # values are present in the data and masked arrays are passed
-    # while ``t.cdf`` emits runtime warnings. This way ``_ttest_finish``
-    # can be shared between the ``stats`` and ``mstats`` versions.
-
-    # TODO need to check the validty of it
-    if alternative == "less":
-        pval = stats.t.sf(df, t)
-    elif alternative == "greater":
-        pval = stats.t.sf(df, -t)
-    elif alternative == "two-sided":
-        pval = stats.t.sf(df, -np.abs(t)) * 2
-    else:
-        raise ValueError("alternative must be " "'less', 'greater' or 'two-sided'")
-
-    if t.ndim == 0:
-        t = t[()]
-    if pval.ndim == 0:
-        pval = pval[()]
-
-    return t, pval
-
-
-def compute_corrected_ttest(
-    differences,
+@xp_capabilities(
+    cpu_only=True,
+    exceptions=["cupy", "jax.numpy"],
+    jax_jit=False,
+    allow_dask_compute=True,
+)
+@_axis_nan_policy_factory(
+    pack_TtestResult,
+    default_axis=0,
+    n_samples=2,
+    result_to_tuple=unpack_TtestResult,
+    n_outputs=6,
+)
+# nan_policy handled by `_axis_nan_policy`, but needs to be left
+# in signature to preserve use as a positional argument
+def ttest_1samp_corrected_NB(
+    a,
+    popmean,
     test_frac=0.1 / 0.9,
     axis=0,
+    nan_policy="propagate",
     alternative="two-sided",
 ):
     """
-    Computes right-tailed paired t-test with corrected variance.
-    This is based on the following code:
-    https://github.com/jpaillard/permucate/blob/3af6ab91edbdf40b62a66fe9a31e45c168050c3a/permucate/utils.py#L221-L252
-
-    Parameters
-    ----------
-    differences : array-like of shape (n_samples,)
-        Vector containing the differences in the score metrics of two models.
-
-    Returns
-    -------
-    t_stat : float
-        Variance-corrected t-statistic.
-    p_val : float
-        Variance-corrected p-value.
+    Modification of https://github.com/scipy/scipy/blob/b1296b9b4393e251511fe8fdd3e58c22a1124899/scipy/stats/_stats_py.py#L6035-L6233
     """
-    df = differences.shape[axis] - 1
+    xp = array_namespace(a)
+    a, axis = _chk_asarray(a, axis, xp=xp)
 
-    mean = np.mean(differences, axis=axis)
-    std = corrected_std(differences, test_frac, axis=axis)
+    n = _length_nonmasked(a, axis)
+    df = n - 1
+
+    if a.shape[axis] == 0:
+        # This is really only needed for *testing* _axis_nan_policy decorator
+        # It won't happen when the decorator is used.
+        NaN = _get_nan(a)
+        return TtestResult(
+            NaN, NaN, df=NaN, alternative=NaN, standard_error=NaN, estimate=NaN
+        )
+
+    mean = xp.mean(a, axis=axis)
+    try:
+        popmean = xp.asarray(popmean)
+        popmean = xp.squeeze(popmean, axis=axis) if popmean.ndim > 0 else popmean
+    except ValueError as e:
+        raise ValueError("`popmean.shape[axis]` must equal 1.") from e
+    d = mean - popmean
+    # Correct the computation of variance
+    v = _var_nadeau_bengio(a, test_frac, axis=axis, ddof=1)
+    denom = xp.sqrt(v / n)
 
     with np.errstate(divide="ignore", invalid="ignore"):
-        t_stat = np.divide(mean, std)
-    t, prob = _ttest_finish(df, t_stat, alternative)
+        t = xp.divide(d, denom)
+        t = t[()] if t.ndim == 0 else t
 
+    dist = _SimpleStudentT(xp.asarray(df, dtype=t.dtype))
+    prob = _get_pvalue(t, dist, alternative, xp=xp)
+    prob = prob[()] if prob.ndim == 0 else prob
+
+    # when nan_policy='omit', `df` can be different for different axis-slices
+    df = xp.broadcast_to(xp.asarray(df), t.shape)
+    df = df[()] if df.ndim == 0 else df
+    # _axis_nan_policy decorator doesn't play well with strings
     alternative_num = {"less": -1, "two-sided": 0, "greater": 1}[alternative]
     return TtestResult(
-        statistic=t,
-        pvalue=prob,
+        t,
+        prob,
         df=df,
         alternative=alternative_num,
-        standard_error=std,
+        standard_error=denom,
         estimate=mean,
+        statistic_np=xp.asarray(t),
+        xp=xp,
     )
---
+++ test/_utils/test_test_statistic.py
@@ -0,0 +1,121 @@
+import pytest
+import warnings
+
+import numpy as np
+import numpy.ma as ma
+import numpy.ma.testutils as ma_npt
+import numpy.testing as npt
+from numpy.ma.testutils import (
+    assert_equal,
+    assert_,
+    assert_allclose,
+    assert_array_equal,
+)
+import scipy.stats.mstats as mstats
+from scipy import stats
+
+from hidimstat.statistical_tools.test_statistic import ttest_1samp_corrected_NB
+
+
+def check_named_results(res, attributes, ma=False, xp=None):
+    """
+    Copy from https://github.com/scipy/scipy/blob/2878daa7083375847e3a181553b146e843efcfad/scipy/stats/tests/common_tests.py#L17
+    """
+    for i, attr in enumerate(attributes):
+        if ma:
+            ma_npt.assert_equal(res[i], getattr(res, attr))
+        else:
+            npt.assert_equal(res[i], getattr(res, attr))
+
+
+class TestTtest_1samp:
+    """
+    Copy from https://github.com/scipy/scipy/blob/2878daa7083375847e3a181553b146e843efcfad/scipy/stats/tests/test_mstats_basic.py#L1455
+    """
+
+    def setup_method(self):
+        self.rng = np.random.default_rng(6043813830)
+
+    def test_vs_nonmasked(self):
+        outcome = self.rng.standard_normal((20, 4)) + [0, 0, 1, 2]
+
+        # 1-D inputs
+        res1 = ttest_1samp_corrected_NB(
+            outcome[:, 0], 1, test_frac=1 - 1 / outcome[:, 0].shape[0]
+        )
+        res2 = mstats.ttest_1samp(outcome[:, 0], 1)
+        assert_allclose(res1, res2)
+        res3 = ttest_1samp_corrected_NB(outcome[:, 0], 1)
+        assert not np.array_equal(res1, res3)
+        assert not np.array_equal(res1, res2)
+
+    def test_fully_masked(self):
+        outcome = ma.masked_array(self.rng.standard_normal(3), mask=[1, 1, 1])
+        expected = (np.nan, np.nan)
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                "ignore", "invalid value encountered in absolute", RuntimeWarning
+            )
+            for pair in [((np.nan, np.nan), 0.0), (outcome, 0.0)]:
+                t, p = ttest_1samp_corrected_NB(*pair)
+                assert_array_equal(p, expected)
+                assert_array_equal(t, expected)
+
+    def test_result_attributes(self):
+        outcome = self.rng.standard_normal((20, 4)) + [0, 0, 1, 2]
+
+        res = ttest_1samp_corrected_NB(outcome[:, 0], 1)
+        attributes = ("statistic", "pvalue")
+        check_named_results(res, attributes, ma=True)
+
+    def test_empty(self):
+        res1 = ttest_1samp_corrected_NB([], 1)
+        assert_(np.all(np.isnan(res1)))
+
+    def test_zero_division(self):
+        t, p = ttest_1samp_corrected_NB([0, 0, 0], 1)
+        assert_equal((np.abs(t), p), (np.inf, 0))
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings(
+                "ignore", "invalid value encountered in absolute", RuntimeWarning
+            )
+            t, p = ttest_1samp_corrected_NB([0, 0, 0], 0)
+            assert_(np.isnan(t))
+            assert_array_equal(p, (np.nan, np.nan))
+
+    @pytest.mark.parametrize("alternative", ["less", "greater"])
+    def test_alternative(self, alternative):
+        x = stats.norm.rvs(loc=10, scale=2, size=100, random_state=123)
+
+        t_ex, p_ex = ttest_1samp_corrected_NB(
+            x, 9, alternative=alternative, test_frac=1 - 1 / x.shape[0]
+        )
+        t, p = mstats.ttest_1samp(x, 9, alternative=alternative)
+        assert_allclose(t, t_ex, rtol=1e-14)
+        assert_allclose(p, p_ex, rtol=1e-14)
+
+        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(x, 9, alternative=alternative)
+        assert not np.array_equal(t_ex_1, t_ex)
+        assert not np.array_equal(t_ex_1, t)
+        assert not np.array_equal(p_ex_1, p_ex)
+        assert not np.array_equal(p_ex_1, p)
+
+        # test with masked arrays
+        x[1:10] = np.nan
+        x = np.ma.masked_array(x, mask=np.isnan(x))
+        t_ex, p_ex = ttest_1samp_corrected_NB(
+            x.compressed(),
+            9,
+            alternative=alternative,
+            test_frac=1 - 1 / (x.shape[0] - 9),
+        )
+        t, p = mstats.ttest_1samp(x, 9, alternative=alternative)
+        assert_allclose(t, t_ex, rtol=1e-14)
+        assert_allclose(p, p_ex, rtol=1e-14)
+
+        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(x, 9, alternative=alternative)
+        assert not np.array_equal(t_ex_1, t_ex)
+        assert not np.array_equal(t_ex_1, t)
+        assert not np.array_equal(p_ex_1, p_ex)
+        assert not np.array_equal(p_ex_1, p)
