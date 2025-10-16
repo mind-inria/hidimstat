1c15c5bd01cca83a7e57b22b24e2b5f31abe1e77
lionel kusch <lionel.a.kusch@inria.fr>
2025-10-16 11:50:34 +0200

    Update src/hidimstat/_utils/utils.py
    Co-authored-by: Joseph Paillard <joseph.paillard@inria.fr>

--- src/hidimstat/_utils/utils.py
+++ src/hidimstat/_utils/utils.py
@@ -166,7 +166,7 @@ def check_statistical_test(test):
     """
     if isinstance(test, str):
         if test == "ttest":
-            return partial(ttest_1samp, axis=1)
+            return partial(ttest_1samp, popmean=0, alternative='greater', axis=1)
         elif test == "wilcoxon":
             return partial(wilcoxon, axis=1)
         else:
