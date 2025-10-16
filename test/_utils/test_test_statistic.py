1d52c0090b32794cec9f3661d0ecb9fd0e1f625c
kusch lionel <lionel.a.kusch@inria.fr>
2025-10-16 17:58:17 +0200

    change default

--- test/_utils/test_test_statistic.py
+++ test/_utils/test_test_statistic.py
@@ -41,13 +41,18 @@ class TestTtest_1samp:
 
         # 1-D inputs
         res1 = ttest_1samp_corrected_NB(
-            outcome[:, 0], 1, test_frac=1 - 1 / outcome[:, 0].shape[0]
+            outcome[:, 0],
+            1,
+            test_frac=1 - 1 / outcome[:, 0].shape[0],
+            alternative="two-sided",
         )
         res2 = mstats.ttest_1samp(outcome[:, 0], 1)
         assert_allclose(res1, res2)
-        res3 = ttest_1samp_corrected_NB(outcome[:, 0], 1)
-        assert not np.array_equal(res1, res3)
-        assert not np.array_equal(res1, res2)
+        res3 = ttest_1samp_corrected_NB(
+            outcome[:, 0], 1, test_frac=0.1, alternative="two-sided"
+        )
+        assert not np.array_equal(res3, res1)
+        assert not np.array_equal(res3, res2)
 
     def test_fully_masked(self):
         outcome = ma.masked_array(self.rng.standard_normal(3), mask=[1, 1, 1])
@@ -56,7 +61,7 @@ class TestTtest_1samp:
             warnings.filterwarnings(
                 "ignore", "invalid value encountered in absolute", RuntimeWarning
             )
-            for pair in [((np.nan, np.nan), 0.0), (outcome, 0.0)]:
+            for pair in [((np.nan, np.nan), 0.0, 0.1), (outcome, 0.0, 0.1)]:
                 t, p = ttest_1samp_corrected_NB(*pair)
                 assert_array_equal(p, expected)
                 assert_array_equal(t, expected)
@@ -64,23 +69,23 @@ class TestTtest_1samp:
     def test_result_attributes(self):
         outcome = self.rng.standard_normal((20, 4)) + [0, 0, 1, 2]
 
-        res = ttest_1samp_corrected_NB(outcome[:, 0], 1)
+        res = ttest_1samp_corrected_NB(outcome[:, 0], 1, 0.1)
         attributes = ("statistic", "pvalue")
         check_named_results(res, attributes, ma=True)
 
     def test_empty(self):
-        res1 = ttest_1samp_corrected_NB([], 1)
+        res1 = ttest_1samp_corrected_NB([], 1, 0.1)
         assert_(np.all(np.isnan(res1)))
 
     def test_zero_division(self):
-        t, p = ttest_1samp_corrected_NB([0, 0, 0], 1)
+        t, p = ttest_1samp_corrected_NB([0, 0, 0], 1, 0.1, alternative="two-sided")
         assert_equal((np.abs(t), p), (np.inf, 0))
 
         with warnings.catch_warnings():
             warnings.filterwarnings(
                 "ignore", "invalid value encountered in absolute", RuntimeWarning
             )
-            t, p = ttest_1samp_corrected_NB([0, 0, 0], 0)
+            t, p = ttest_1samp_corrected_NB([0, 0, 0], 0, 0.1)
             assert_(np.isnan(t))
             assert_array_equal(p, (np.nan, np.nan))
 
@@ -89,13 +94,15 @@ class TestTtest_1samp:
         x = stats.norm.rvs(loc=10, scale=2, size=100, random_state=123)
 
         t_ex, p_ex = ttest_1samp_corrected_NB(
-            x, 9, alternative=alternative, test_frac=1 - 1 / x.shape[0]
+            x, 9, test_frac=1 - 1 / x.shape[0], alternative=alternative
         )
         t, p = mstats.ttest_1samp(x, 9, alternative=alternative)
         assert_allclose(t, t_ex, rtol=1e-14)
         assert_allclose(p, p_ex, rtol=1e-14)
 
-        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(x, 9, alternative=alternative)
+        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(
+            x, 9, test_frac=0.1, alternative=alternative
+        )
         assert not np.array_equal(t_ex_1, t_ex)
         assert not np.array_equal(t_ex_1, t)
         assert not np.array_equal(p_ex_1, p_ex)
@@ -107,14 +114,16 @@ class TestTtest_1samp:
         t_ex, p_ex = ttest_1samp_corrected_NB(
             x.compressed(),
             9,
-            alternative=alternative,
             test_frac=1 - 1 / (x.shape[0] - 9),
+            alternative=alternative,
         )
         t, p = mstats.ttest_1samp(x, 9, alternative=alternative)
         assert_allclose(t, t_ex, rtol=1e-14)
         assert_allclose(p, p_ex, rtol=1e-14)
 
-        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(x, 9, alternative=alternative)
+        t_ex_1, p_ex_1 = ttest_1samp_corrected_NB(
+            x, 9, test_frac=0.1, alternative=alternative
+        )
         assert not np.array_equal(t_ex_1, t_ex)
         assert not np.array_equal(t_ex_1, t)
         assert not np.array_equal(p_ex_1, p_ex)
