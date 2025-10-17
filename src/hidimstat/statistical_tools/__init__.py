c106e6cfec771e05b7b9f07f3f26649afae8f54b
jpaillard <joseph.paillard@inria.fr>
2025-10-17 11:01:00 +0200

    add test and init

--- src/hidimstat/statistical_tools/__init__.py
+++ src/hidimstat/statistical_tools/__init__.py
@@ -1,7 +1,9 @@
 from .conditional_sampling import ConditionalSampler
 from .gaussian_knockoffs import GaussianKnockoffs
+from .test_statistic import ttest_1samp_corrected_NB
 
 __all__ = [
     "ConditionalSampler",
     "GaussianKnockoffs",
+    "ttest_1samp_corrected_NB",
 ]
