3298f2863233b26d5b0f5bf58c8192bbe34e5310
lionel kusch <lionel.a.kusch@inria.fr>
2025-10-24 17:30:46 +0200

    Update src/hidimstat/ensemble_clustered_inference.py
    Co-authored-by: bthirion <bertrand.thirion@inria.fr>

--- src/hidimstat/ensemble_clustered_inference.py
+++ src/hidimstat/ensemble_clustered_inference.py
@@ -169,7 +169,7 @@ def clustered_inference(
     X_init : ndarray, shape (n_samples, n_features)
         Original high-dimensional input data matrix.
 
-    y : ndarray, shape (n_samples,) or (n_samples, n_task)
+    y : ndarray, shape (n_samples,) or (n_samples, n_tasks)
         Target variable(s). Can be univariate or multivariate (temporal) data.
 
     ward : sklearn.cluster.FeatureAgglomeration
