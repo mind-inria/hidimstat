fd4ee191f9b10a1051f45978682a27adea5e5e90
jpaillard <joseph.paillard@inria.fr>
2025-11-11 09:24:30 +0100

    debug test

--- src/hidimstat/conditional_feature_importance.py
+++ src/hidimstat/conditional_feature_importance.py
@@ -396,4 +396,5 @@ class CFImportanceCV(BasePerturbationCV):
             n_jobs=1,  # no parallelization inside the fold
         )
         cfi.fit(X_train, y_train)
+        1 / 0
         return cfi
