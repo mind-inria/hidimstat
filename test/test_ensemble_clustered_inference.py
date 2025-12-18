36fde26f23429bcb26de6fcd23b4cbd5ac78692a
bthirion <bertrand.thirion@inria.fr>
2025-12-18 13:55:09 +0100

    fixing one failure

--- test/test_ensemble_clustered_inference.py
+++ test/test_ensemble_clustered_inference.py
@@ -325,7 +325,8 @@ def test_encludl_independence():
         n_clusters=n_clusters, connectivity=connectivity, linkage="ward"
     )
     encludl = EnCluDL(
-        desparsified_lasso=DesparsifiedLasso(),
+        desparsified_lasso=DesparsifiedLasso(
+            estimator=MultiTaskLassoCV(max_iter=1000)),
         clustering=ward,
         n_bootstraps=20,
         cluster_bootstrap_size=0.5,
