7a374409ce5b292bcba192f2daf256875f80292e
jpaillard <joseph.paillard@inria.fr>
2026-01-08 11:14:13 +0100

    cleanup

--- test/test_version.py
+++ test/test_version.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 import hidimstat
 
 
@@ -7,8 +5,3 @@ def test_version():
     """Test that the version is set correctly."""
     assert isinstance(hidimstat.__version__, str)
     assert len(hidimstat.__version__.split(".")) >= 3
-
-
-def test_timeout():
-    sleep(65)
-    assert True
