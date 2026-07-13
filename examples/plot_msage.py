e173c633b57e3f1815c44b82f53833d116f0bf02
jpaillard <joseph.paillard@roche.com>
2026-07-13 15:28:21 +0000

    keep only LGBM

--- examples/plot_msage.py
+++ examples/plot_msage.py
@@ -11,52 +11,23 @@ This is controlled by the `n_subsets` parameter. Finally, the expectation over
 the marginal distribution is also approximated using `n_permutations`.
 """
 
-# %%
-# Ridge regression example on the diabetes dataset
-# ------------------------------------------------
-# We start by loading the diabetes dataset and fitting a ridge regression
-# model. We then compute the SAGE values for each feature and plot the results.
-
-import numpy as np
-from sklearn.datasets import load_diabetes
-from sklearn.linear_model import RidgeCV
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
-
-from hidimstat import SAGE
-
-dataset = load_diabetes()
-X, y = dataset.data, dataset.target
-feature_names = dataset.feature_names
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-model = RidgeCV()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-print("R2 score:", r2_score(y_test, y_pred))
-
-sage = SAGE(
-    model,
-    n_subsets=512,
-    n_permutations=100,
-    random_state=0,
-    n_jobs=8,
-)
-sage.fit(X_train)
-sage.importance(X_test, y_test)
-ax = sage.plot_importance(feature_names=feature_names)
-
 
 # %%
 # LightGBM example on the bike sharing dataset
 # --------------------------------------------
-# We then demonstrate how to use SAGE on a larger dataset, the bike sharing
+# We demonstrate how to use SAGE on the bike sharing
 # dataset. We fit a LightGBM model and compute the SAGE values for each
 # feature. To make the computation tractable, we subsample the test set to
 # 1024 samples.
 
+import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
+
+from hidimstat import SAGE
 
 bike_sharing = fetch_openml("Bike_Sharing_Demand", version=2, as_frame=True)
 df = bike_sharing.frame
@@ -83,4 +54,7 @@ subsample_size = 1024
 rng = np.random.default_rng(0)
 subsample_ids = rng.choice(len(X_test), size=subsample_size, replace=False)
 sage.importance(X_test[subsample_ids], y_test[subsample_ids])
-sage.plot_importance(feature_names=df.drop(columns=["count"]).columns.tolist())
+sage.plot_importance(
+    feature_names=df.drop(columns=["count"]).columns.tolist(),
+    color="tab:purple",
+)
