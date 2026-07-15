ca1be8f43d796d70214d703a3d917f65c4bebd7e
jpaillard <joseph.paillard@roche.com>
2026-07-15 15:31:59 +0000

    add conclusion plus correlation explanation

--- examples/plot_msage.py
+++ examples/plot_msage.py
@@ -2,7 +2,7 @@
 Shapley Additive Global Importance (SAGE) example
 =================================================
 
-In this example, we demonstrate how to use measure feature importance using
+In this example, we demonstrate how to measure feature importance using
 SAGE :footcite:t:`Covert2020` on the diabetes dataset. For this example, we use
 the marginal version of SAGE, which limits the computational cost. To further
 reduce the computational cost, Shapley values are estimated using a Monte Carlo
@@ -16,7 +16,7 @@ the marginal distribution is also approximated using `n_permutations`.
 # LightGBM example on the bike sharing dataset
 # --------------------------------------------
 # We demonstrate how to use SAGE on the bike sharing
-# dataset. We fit a LightGBM model and compute its prediction $R^2$ score on a
+# dataset. We fit a HistGradientBoostingRegressor model and compute its $R^2$ score on a
 # held-out test set.
 
 import numpy as np
@@ -48,6 +48,8 @@ print("R2 score:", r2_score(y_test, y_pred))
 # SAGE values. Finally, the SAGE values are plotted using the `plot_importance`
 # function.
 
+import matplotlib.pyplot as plt
+
 from hidimstat import SAGE
 
 sage = SAGE(
@@ -62,7 +64,40 @@ subsample_size = 1024
 rng = np.random.default_rng(0)
 subsample_ids = rng.choice(len(X_test), size=subsample_size, replace=False)
 sage.importance(X_test[subsample_ids], y_test[subsample_ids])
-sage.plot_importance(
+ax = sage.plot_importance(
     feature_names=df.drop(columns=["count"]).columns.tolist(),
     color="tab:purple",
 )
+ax.semilogx()
+plt.tight_layout()
+plt.show()
+
+
+# %%
+# This analysis reveals that the hour of the day is the most important feature
+# for predicting bike sharing demand. This example also illustrates a specific
+# property of SAGE: it tends to distribute importance over correlated features.
+# This can be seen for the temperature and "feel temperature" features,
+# which have a near-perfect correlation, as seen in the correlation matrix below,
+# and are both assigned similar importance.
+# This property is a consequence of the axioms of Shapley values, and contrasts
+# with other feature importance methods, such as
+# :class:`~hidimstat.LOCO` or :class:`~hidimstat.CFI`.
+
+corr_mat = np.corrcoef(X_train, rowvar=False)
+fig, ax = plt.subplots()
+ax.imshow(corr_mat, cmap="coolwarm", vmin=-1, vmax=1)
+ax.set_xticks(np.arange(corr_mat.shape[0]))
+ax.set_yticks(np.arange(corr_mat.shape[0]))
+ax.set_xticklabels(
+    df.drop(columns=["count"]).columns.tolist(), rotation=45, ha="right"
+)
+ax.set_yticklabels(df.drop(columns=["count"]).columns.tolist())
+cbar = ax.figure.colorbar(
+    ax.imshow(corr_mat, cmap="coolwarm", vmin=-1, vmax=1), label="correlation"
+)
+
+# %%
+# References
+# ----------
+# .. footbibliography::
