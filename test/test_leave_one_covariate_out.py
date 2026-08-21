666056d92ec58ab9c59571b3c3ec53fd6c5fdaaf
Marc Hulcelle <marc.hulcelle@inria.fr>
2026-08-21 14:55:28 +0200

    [ENH] Speed up tests (#739)
    * first round of improvement on vim tests
    
    * sped up cfi and dcrt tests
    
    * Fixed empty test CFI/test_function_cfi
    
    * Failing test KO/test_estimate_distribution fix
    
    * Relaxing EnCluDL/test_cludl_independence test
    
    * removed useless np.where operations
    
    * removed n_jobs=-1 since tests are parallelized with xdist
    
    * Switched to binary mask for fixture data_generator
    
    * Update test/test_conditional_feature_importance.py
    
    ---------
    
    Co-authored-by: bthirion <bertrand.thirion@inria.fr>

--- test/test_leave_one_covariate_out.py
+++ test/test_leave_one_covariate_out.py
@@ -15,18 +15,14 @@ from hidimstat.base_perturbation import BasePerturbation
 from hidimstat.statistical_tools.multiple_testing import fdp_power
 
 
-def test_loco():
+@pytest.mark.parametrize(
+    "n_samples, n_features, support_size, rho, seed, value, signal_noise_ratio, rho_serial",
+    [(100, 20, 4, 0, 42, 1.0, 10.0, 0.0)],
+    ids=["basic data"],
+)
+def test_loco(data_generator):
     """Test the Leave-One-Covariate-Out algorithm on a linear scenario."""
-    X, y, beta, _ = multivariate_simulation(
-        n_samples=150,
-        n_features=200,
-        support_size=10,
-        shuffle=False,
-        seed=42,
-    )
-    important_features = np.where(beta != 0)[0]
-    non_important_features = np.where(beta == 0)[0]
-
+    X, y, important_features = data_generator
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
     regression_model = LinearRegression()
@@ -36,7 +32,6 @@ def test_loco():
         estimator=regression_model,
         method="predict",
         features_groups=None,
-        n_jobs=1,
     )
 
     loco.fit(
@@ -48,13 +43,14 @@ def test_loco():
     assert importance.shape == (X.shape[1],)
     assert (
         importance[important_features].mean()
-        > importance[non_important_features].mean()
+        > importance[~important_features].mean()
     )
 
     # Same with groups and a pd.DataFrame
+    feature_ids = np.arange(X.shape[1])
     groups = {
-        "group_0": [f"col_{i}" for i in important_features],
-        "the_group_1": [f"col_{i}" for i in non_important_features],
+        "group_0": [f"col_{i}" for i in feature_ids[important_features]],
+        "the_group_1": [f"col_{i}" for i in feature_ids[~important_features]],
     }
     X_df = pd.DataFrame(X, columns=[f"col_{i}" for i in range(X.shape[1])])
     X_train_df, X_test_df, y_train, y_test = train_test_split(
@@ -76,7 +72,7 @@ def test_loco():
     assert importance[0].mean() > importance[1].mean()
 
     # Classification case
-    y_clf = np.where(y > np.median(y), 1, 0)
+    y_clf = (y > np.median(y)).astype(int)
     _, _, y_train_clf, y_test_clf = train_test_split(X, y_clf, random_state=0)
     logistic_model = LogisticRegression()
     logistic_model.fit(X_train, y_train_clf)
@@ -85,8 +81,8 @@ def test_loco():
         estimator=logistic_model,
         method="predict_proba",
         features_groups={
-            "group_0": important_features,
-            "the_group_1": non_important_features,
+            "group_0": feature_ids[important_features],
+            "the_group_1": feature_ids[~important_features],
         },
         loss=log_loss,
         n_jobs=1,
@@ -104,9 +100,9 @@ def test_loco():
 def test_raises_value_error():
     """Test for error when model does not have predict_proba or predict."""
     X, y, _, _ = multivariate_simulation(
-        n_samples=150,
-        n_features=200,
-        support_size=10,
+        n_samples=100,
+        n_features=20,
+        support_size=4,
         shuffle=False,
         seed=42,
     )
@@ -145,18 +141,14 @@ def test_raises_value_error():
         loco.importance(X, y)
 
 
-def test_loco_function():
+@pytest.mark.parametrize(
+    "n_samples, n_features, support_size, rho, seed, value, signal_noise_ratio, rho_serial",
+    [(100, 20, 4, 0, 42, 1.0, 10.0, 0.0)],
+    ids=["basic data"],
+)
+def test_loco_function(data_generator):
     """Test the function of LOCO algorithm on a linear scenario."""
-    X, y, beta, _ = multivariate_simulation(
-        n_samples=150,
-        n_features=100,
-        support_size=10,
-        shuffle=False,
-        seed=42,
-    )
-    important_features = np.where(beta != 0)[0]
-    non_important_features = np.where(beta == 0)[0]
-
+    X, y, important_features = data_generator
     X_train, _, y_train, _ = train_test_split(X, y, random_state=0)
 
     regression_model = LinearRegression()
@@ -167,13 +159,12 @@ def test_loco_function():
         X,
         y,
         method="predict",
-        n_jobs=1,
     )
 
     assert importance.shape == (X.shape[1],)
     assert (
         importance[important_features].mean()
-        > importance[non_important_features].mean()
+        > importance[~important_features].mean()
     )
 
 
@@ -191,22 +182,21 @@ def test_loco_cv(data_generator):
     Note: even though the only the expected FDP should be controlled, in practice
     the simulation setting is simple enough to satisfy this stronger condition.
     """
-    X, y, important_features, _ = data_generator
+    X, y, important_features = data_generator
 
     model = RidgeCV()
     cv = KFold(n_splits=5, shuffle=True, random_state=0)
     loco_cv = LOCOCV(
         estimators=model,
         cv=cv,
-        n_jobs=5,
     )
     loco_cv.fit(X, y)
     loco_cv.importance(X, y)
 
     alpha = 0.1
     selected = loco_cv.fdr_selection(fdr=alpha)
-    gt_mask = np.zeros(X.shape[1], dtype=int)
-    gt_mask[important_features] = 1
-    fdp, power = fdp_power(selected=selected, ground_truth=gt_mask)
+    fdp, power = fdp_power(
+        selected=selected, ground_truth=important_features.astype(int)
+    )
     assert fdp < alpha
     assert power > 0.8
