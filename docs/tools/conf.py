4db1bfba1425f8af5d5869735ce7b14805f11108
Remi Gau <remi_gau@hotmail.com>
2026-09-14 14:19:55 +0200

    copy multiline

--- docs/tools/conf.py
+++ docs/tools/conf.py
@@ -126,7 +126,11 @@ if _citation_path.exists():
     )
 
 # Specify how to identify the prompt when copying a code snippet
-copybutton_prompt_text = r">>> |\.\.\. "
+# We want avoid the copy button to only take
+# the first line of a multiline input.
+copybutton_prompt_text = (
+    r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+)
 copybutton_prompt_is_regexp = True
 copybutton_exclude = "style"
 
