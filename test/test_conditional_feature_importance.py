48b5761333f652515057772abc29759a1194bf0a
Remi Gau <remi_gau@hotmail.com>
2026-09-15 13:54:52 +0200

    no sklearn < 1.6

--- test/test_conditional_feature_importance.py
+++ test/test_conditional_feature_importance.py
@@ -25,7 +25,7 @@ from hidimstat.base_perturbation import BasePerturbation
 from hidimstat.conditional_feature_importance import CFI, CFICV, cfi_importance
 from hidimstat.statistical_tools.multiple_testing import fdp_power
 
-from .conftest import SKLEARN_LT_1_6, check_estimator, fitted_linear_regression
+from .conftest import fitted_linear_regression
 
 
 def fitted_ridged_cv() -> RidgeCV:
@@ -123,40 +123,12 @@ def expected_failed_checks(estimator):
         return failed_checks
 
 
-if SKLEARN_LT_1_6:
-
-    @pytest.mark.parametrize(
-        "estimator, check, name",
-        check_estimator(
-            estimators=ESTIMATORS_TO_CHECK,
-            return_expected_failed_checks=expected_failed_checks,
-        ),
-    )
-    def test_check_estimator_sklearn_valid(estimator, check, name):  # noqa: ARG001
-        """Check compliance with sklearn estimators."""
-        check(estimator)
-
-    @pytest.mark.xfail(reason="invalid checks should fail")
-    @pytest.mark.parametrize(
-        "estimator, check, name",
-        check_estimator(
-            estimators=ESTIMATORS_TO_CHECK,
-            valid=False,
-            return_expected_failed_checks=expected_failed_checks,
-        ),
-    )
-    def test_check_estimator_sklearn_invalid(estimator, check, name):  # noqa: ARG001
-        """Check compliance with sklearn estimators."""
-        check(estimator)
-
-else:
-
-    @parametrize_with_checks(
-        estimators=ESTIMATORS_TO_CHECK,
-        expected_failed_checks=expected_failed_checks,
-    )
-    def test_check_estimator_sklearn(estimator, check):
-        check(estimator)
+@parametrize_with_checks(
+    estimators=ESTIMATORS_TO_CHECK,
+    expected_failed_checks=expected_failed_checks,
+)
+def test_check_estimator_sklearn(estimator, check):
+    check(estimator)
 
 
 def run_cfi(X, y, n_permutation, seed):
